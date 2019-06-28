683bd9f4992b0e07abbc7760f6e47913ddaa014f
Ryan Faircloth <rfaircloth@splunk.com>
2019-06-28 08:35:54 -0400

    Clean build of syslog-docker

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -3,6 +3,6 @@
 #Run syslog
 mkdir /opt/syslog-ng/var
 rm /opt/syslog-ng/var/syslog-ng.ctl
-/opt/syslog-ng/bin/switch_transport.sh hec
+/opt/syslog-ng/sbin/switch_transport.sh hec
 /opt/syslog-ng/sbin/syslog-ng --process-mode=background
 /opt/syslog-ng/sbin/splunkmetrics.sh 
