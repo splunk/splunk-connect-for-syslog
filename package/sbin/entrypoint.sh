69e1b0da443a22fa35969e9bfa818b0efc9e70f3
Ryan Faircloth <rfaircloth@splunk.com>
2019-06-28 09:18:31 -0400

    Build with healthcheck

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -1,8 +1,8 @@
-#!/usr/bin/dumb-init /bin/sh
+#!/opt/rh/rh-python36/root/usr/bin/dumb-init /bin/bash
 
 #Run syslog
 mkdir /opt/syslog-ng/var
-rm /opt/syslog-ng/var/syslog-ng.ctl
-/opt/syslog-ng/sbin/switch_transport.sh hec
+rm /opt/syslog-ng/var/syslog-ng.ctl || true
+/opt/syslog-ng/sbin/switch_transport.sh
 /opt/syslog-ng/sbin/syslog-ng --process-mode=background
 /opt/syslog-ng/sbin/splunkmetrics.sh 
