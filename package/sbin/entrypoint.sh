c59c14e7b5819550ed03f1a6a15a64fc3b7b5855
Ryan Faircloth <rfaircloth@splunk.com>
2019-06-27 22:22:17 -0400

    Functional build

---
+++ package/sbin/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/dumb-init /bin/sh
+
+#Run syslog
+mkdir /opt/syslog-ng/var
+rm /opt/syslog-ng/var/syslog-ng.ctl
+/opt/syslog-ng/bin/switch_transport.sh hec
+/opt/syslog-ng/sbin/syslog-ng --process-mode=background
+/opt/syslog-ng/sbin/splunkmetrics.sh 
