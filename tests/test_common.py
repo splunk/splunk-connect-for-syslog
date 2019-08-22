90810f8267639ee903be0e5692fc28271bba82bb
Ryan Faircloth <rfaircloth@splunk.com>
2019-08-22 07:23:17 -0400

    Revert "remove old filters"
    This reverts commit 1f994134784548936709a8f2ba70398545514d93.

--- tests/test_common.py
+++ tests/test_common.py
@@ -18,7 +18,7 @@ env = Environment(extensions=['jinja2_time.TimeExtension'])
 def test_defaultroute(record_property, setup_wordlist, setup_splunk):
     host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
 
-    mt = env.from_string("{{ mark }} {% now 'utc', '%b %d %H:%M:%S' %} {{ host }} sc4sdefault[0]: test\n")
+    mt = env.from_string("{{ mark }}{% now 'utc', '%b %d %H:%M:%S' %} {{ host }} sc4sdefault[0]: test\n")
     message = mt.render(mark="<111>", host=host)
 
     sendsingle(message)
@@ -38,7 +38,7 @@ def test_defaultroute(record_property, setup_wordlist, setup_splunk):
 def test_tag(record_property, setup_wordlist, setup_splunk):
     host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
 
-    mt = env.from_string("{{ mark }} {% now 'utc', '%b %d %H:%M:%S' %} testvp-{{ host }} sc4sdefault[0]: test\n")
+    mt = env.from_string("{{ mark }}{% now 'utc', '%b %d %H:%M:%S' %} testvp-{{ host }} sc4sdefault[0]: test\n")
     message = mt.render(mark="<111>", host=host)
 
     sendsingle(message)
