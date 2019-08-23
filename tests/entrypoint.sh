b2e31ecb93e08c530125533e8f9f986b9b2aa5df
Ryan Faircloth <rfaircloth@splunk.com>
2019-08-23 11:02:17 -0400

    Update entrypoint.sh

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
+echo Check for sc4s
 wait-for sc4s:514
-wait-for splunk:9997
+echo check for splunk web
 wait-for splunk:8000
+echo check for splunk web
+wait-for splunk:8000
+echo check for splunk web
+wait-for splunk:8000
+echo check for splunk web
+wait-for splunk:8000
+
+echo check for splunk mgmt
 wait-for splunk:8089
+echo check for splunk mgmt
+wait-for splunk:8089
+echo check for splunk mgmt
+wait-for splunk:8089
+echo check for splunk mgmt
+wait-for splunk:8089
+
+echo check for splunk hec
+wait-for splunk:8088
+echo check for splunk hec
+wait-for splunk:8088
+echo check for splunk hec
+wait-for splunk:8088
+echo check for splunk hec
 wait-for splunk:8088
 
 cd /work;python -m pytest --junitxml=/work/test-results/functional/functional.xml
