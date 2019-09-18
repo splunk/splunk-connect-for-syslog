55f545878a650c3c9e34211454b6f4519a859183
ryan faircloth <rfaircloth@splunk.com>
2019-09-18 14:17:31 -0400

    directly run syslog-ng

--- package/sbin/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/opt/rh/rh-python36/root/usr/bin/dumb-init /bin/bash
-#Copyright 2019 Splunk, Inc.
-#
-#Use of this source code is governed by a BSD-2-clause-style
-#license that can be found in the LICENSE-BSD2 file or at
-#https://opensource.org/licenses/BSD-2-Clause
-#Run syslog
-dockerize
-/opt/syslog-ng/sbin/syslog-ng -F
