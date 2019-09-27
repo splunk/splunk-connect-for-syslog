1b7309308bd9fd559ffebe8e4ac846e890b9cdad
Mark Bonsack <mbonsack@splunk.com>
2019-09-27 15:59:57 -0700

    Proofpoint log_path and filter

--- tests/test_proofpoint.py
+++ tests/test_proofpoint.py
@@ -13,7 +13,7 @@ env = Environment(extensions=['jinja2_time.TimeExtension'])
 
 
 # Apr 17 18:33:26 aplegw01 filter_instance1[195529]: rprt s=2hdryp02r6 m=1 x=2hdryp02r6-1 cmd=send profile=mail qid=w3HMWjG3039079 rcpts=rfaircloth@splunk.com
-def test_proofpoint(record_property, setup_wordlist, get_host_key, setup_splunk):
+def test_proofpoint_pps_filter(record_property, setup_wordlist, get_host_key, setup_splunk):
     host = get_host_key
 
     mt = env.from_string(
@@ -22,7 +22,7 @@ def test_proofpoint(record_property, setup_wordlist, get_host_key, setup_splunk)
 
     sendsingle(message)
 
-    st = env.from_string("search index=email host=\"{{ host }}\" sourcetype=\"proofpoint:pps\" | head 2")
+    st = env.from_string("search index=email host=\"{{ host }}\" sourcetype=\"pps_filter_log\" | head 2")
     search = st.render(host=host)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
@@ -33,17 +33,17 @@ def test_proofpoint(record_property, setup_wordlist, get_host_key, setup_splunk)
 
     assert resultCount == 1
 
-# Apr 17 18:33:26 aplegw01 queued-alert[195529]: info s=2hdryp02r6 m=1 x=2hdryp02r6-1 cmd=send profile=mail qid=w3HMWjG3039079 rcpts=rfaircloth@splunk.com
-def test_proofpoint_no_rprt(record_property, setup_wordlist, get_host_key, setup_splunk):
+# Apr 17 18:35:26 aplegw02 sendmail[56106]: w3HMZPVT056101: to=<rfaircloth@splunk.com>, delay=00:00:01, xdelay=00:00:01, mailer=esmtp, tls_verify=FAIL, pri=133527, relay=mx1.splunk.iphmx.com. [216.71.153.223], dsn=2.0.0, stat=Sent (ok:  Message 22675962 accepted)
+def test_proofpoint_pps_mail(record_property, setup_wordlist, get_host_key, setup_splunk):
     host = get_host_key
 
     mt = env.from_string(
-        "{{ mark }} {% now 'utc', '%b %d %H:%M:%S' %} {{ host }} queued-alert[195529]: info s=2hdryp02r6 m=1 x=2hdryp02r6-1 cmd=send profile=mail qid=w3HMWjG3039079 rcpts=rfaircloth@splunk.com\n")
+        "{{ mark }} {% now 'utc', '%b %d %H:%M:%S' %} pps-{{ host }} sendmail[195529]: w3HMZPVT056101: to=<rfaircloth@splunk.com>, delay=00:00:01, xdelay=00:00:01, mailer=esmtp, tls_verify=FAIL, pri=133527, relay=mx1.splunk.iphmx.com. [216.71.153.223], dsn=2.0.0, stat=Sent (ok:  Message 22675962 accepted)\n")
     message = mt.render(mark="<166>", host=host)
 
     sendsingle(message)
 
-    st = env.from_string("search index=email host=\"{{ host }}\" sourcetype=\"proofpoint:norprt\" | head 2")
+    st = env.from_string("search index=email host=\"pps-{{ host }}\" sourcetype=\"pps_mail_log\" | head 2")
     search = st.render(host=host)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
@@ -53,3 +53,4 @@ def test_proofpoint_no_rprt(record_property, setup_wordlist, get_host_key, setup
     record_property("message", message)
 
     assert resultCount == 1
+
