61a21c26b5ab1b5b3450e87b791144b36310ad6f
rfaircloth-splunk <rfaircloth@splunk.com>
2019-11-10 16:46:23 -0500

    wip

--- tests/test_ubiquiti_unifi.py
+++ tests/test_ubiquiti_unifi.py
@@ -13,7 +13,7 @@ from .splunkutils import *
 env = Environment(extensions=['jinja2_time.TimeExtension'])
 #<27>Nov  8 17:28:43 US8P60,18e8294876c3,v4.0.66.10832 switch: DOT1S: dot1sBpduReceive(): Discarding the BPDU on port 0/7, since it is an invalid BPDU type
 
-def test_ubiquiti_unifi(record_property, setup_wordlist, setup_splunk):
+def test_ubiquiti_unifi_us8p60(record_property, setup_wordlist, setup_splunk):
     host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
 
     mt = env.from_string(
@@ -21,7 +21,47 @@ def test_ubiquiti_unifi(record_property, setup_wordlist, setup_splunk):
     message = mt.render(mark="<27>", host=host)
     sendsingle(message)
 
-    st = env.from_string("search index=netops sourcetype=ubnt earliest=-2m | head 2")
+    st = env.from_string("search index=netops sourcetype=ubnt:switch earliest=-2m | head 2")
+    search = st.render(host=host)
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
+
+#<29>Nov 10 20:46:02 US24P250,f09fc26f4419,v4.0.54.10625 switch: TRAPMGR: Cold Start: Unit: 0
+def test_ubiquiti_unifi_switch_us24p250(record_property, setup_wordlist, setup_splunk):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    mt = env.from_string(
+        "{{mark}}{% now 'utc', '%b %d %H:%M:%S' %} US24P250,f09fc26f4419,v4.0.54.10625 switch: TRAPMGR: Cold Start: Unit: 0")
+    message = mt.render(mark="<27>", host=host)
+    sendsingle(message)
+
+    st = env.from_string("search index=netops sourcetype=ubnt:switch earliest=-2m | head 2")
+    search = st.render(host=host)
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
+
+#<30>Nov 10 11:49:46 U7PG2,788a2056b181,v4.0.66.10832: logread[5495]: Logread connected to 10.2.0.9:514
+def test_ubiquiti_unifi_ap_u7pg2(record_property, setup_wordlist, setup_splunk):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    mt = env.from_string(
+        "{{mark}}{% now 'utc', '%b %d %H:%M:%S' %} U7PG2,788a2056b181,v4.0.66.10832: logread[5495]: Logread connected to 10.2.0.9:514")
+    message = mt.render(mark="<27>", host=host)
+    sendsingle(message)
+
+    st = env.from_string("search index=netops sourcetype=ubnt:wireless earliest=-2m | head 2")
     search = st.render(host=host)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
