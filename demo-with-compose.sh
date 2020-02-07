58719b60f8aa9ba7779261303a43e9b49985b9d2
rfaircloth-splunk <rfaircloth@splunk.com>
2020-02-07 10:28:02 -0500

    cleanup

--- demo-with-compose.sh
+++ demo-with-compose.sh
@@ -4,7 +4,7 @@
 #Use of this source code is governed by a BSD-2-clause-style
 #license that can be found in the LICENSE-BSD2 file or at
 #https://opensource.org/licenses/BSD-2-Clause
-docker-compose -f tests/docker-compose-ci.yml build
-docker-compose -f tests/docker-compose-ci.yml up
+docker-compose -f tests/docker-compose-script.yml build
+docker-compose -f tests/docker-compose-script.yml up
 
 EXIT=$0
