af13c5351e88787df9c94b28d453df6d34b7c26e
rfaircloth-splunk <rfaircloth@splunk.com>
2020-04-07 20:41:11 -0400

    Resolve double -F

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -54,7 +54,7 @@ echo sc4s version=$(cat /VERSION) >/opt/syslog-ng/var/log/syslog-ng.out
 echo syslog-ng starting
 /opt/syslog-ng/bin/persist-tool add /opt/syslog-ng/etc/reset_persist -o /opt/syslog-ng/var
 
-/opt/syslog-ng/sbin/syslog-ng -F $@ &
+/opt/syslog-ng/sbin/syslog-ng $@ &
 pid="$!"
 # wait forever
 while true
