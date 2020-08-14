5bd9897fcbec4cd93b191c32e88b52c2a356bd24
Tushar Balar <tbalar@splunk.com>
2020-08-14 20:08:31 +0530

    ADDON-28629: Updated authentication filter and test case

--- tests/test_cisco_esa.py
+++ tests/test_cisco_esa.py
@@ -24,8 +24,8 @@ testdata_http = [
 ];
 
 testdata_cef = [
-    '{{ bsd }} {{ host }} {{ app }}: CEF:0|Cisco|C100V Email Security Virtual Appliance|13.0.0-283|ESA_CONSOLIDATED_LOG_EVENT|Consolidated Log Event|5|cs6Label=SDRRepScore cs6=Weak deviceExternalId=111111111111-ZZZZZZZZZZZ ESAMID=19 startTime=Mon Aug 10 10:10:07 2020 deviceOutboundInterface=OutgoingMail ESADMARCVerdict=PermFailure dvc=1.1.1.1 ESAAttachmentDetails={\'sample_ESA_attachment\':   {\'AMP\': {\'Verdic\': \'FILE UNKNOWN\', \'fileHash\': \'35918c38cea9ad9279ad0b206091be3085874528edaf6994b73f0758679815a3\'},  \'BodyScanner\': {}}} ESAFriendlyFrom=sample_user deviceDirection=1 ESAMailFlowPolicy=ACCEPT suser=sample_user cs1Label=MailPolicy cs1=DEFAULT act=BOUNCED ESAFinalActionDetails=To SPAM cs4Label=ExternalMsgID cs4=\'<dummy@cs4>\' duser=dummy_duser ESAHeloIP=10.0.0.1 cfp1Label=SBRSScore cfp1=None ESASDRDomainAge=83 years 10 months 26 days cs3Label=SDRThreatCategory cs3=N/A ESASPFVerdict=PermError sourceHostName=unknown ESASenderGroup=SUSPECTLIST sourceAddress=5001:0db8:85a3:2222:6565:7457:0370:5453 ESAICID=30 cs5Label=ESAMsgLanguage cs5=English msg=[Cousin\=20Domain][SUSPECTED\=20SPAM]\=20asdfdsaf cs2Label=GeoLocation cs2=India ESAMsgTooBigFromSender=true ESARateLimitedIP=10.0.0.2 ESADHASource=10.0.0.3 ESAHeloDomain=test.com ESATLSOutConnStatus=Success ESATLSOutProtocol=TLSv1.2 ESATLSOutCipher=ECDHE-RSA-AES128-GCM-SHA256 ESATLSInConnStatus=Success ESATLSInProtocol=TLSv1.2 ESATLSInCipher=ECDHE-RSA-AES128-GCM-SHA256 ESADKIMVerdict=PermError ESAReplyTo=demo@test.com ESAASVerdict=SUSPECT ESAAMPVerdict=FA_PENDING ESAAVVerdict=REPAIRED ESAGMVerdict=POSITIVE ESACFVerdict=MATCH ESAOFVerdict=NEGATIVE ESADLPVerdict=NO VIOLATION ESAURLDetails={url1:{expanded_url: sample_expanded_url, category: dummy_category, wbrs_score: 62, in_attachment: sample_attachment_file, Attachment_with_url: www.sample.attachment.url.com,},url2:{…}} ESAMARAction= {action:success;succesful_rcpts=56;failed_recipients=0;filename=dummy_filename.txt} Message Filters Verdict=MATCH ESADCID=507 EndTime=Mon Aug 10 10:10:07 2020 ESADaneStatus=success ESADaneHost=testdomain.com',
-    '{{ bsd }} {{ host }} {{ app }}: CEF:0|Cisco|C190 Email Security Appliance|13.5.0-135|ESA_CONSOLIDATED_LOG_EVENT|Consolidated Log Event|5|deviceExternalId=111111111111-ZZZZZZZZZZZ ESAMID=99 ESAICID=27 ESAAMPVerdict=SKIPPED ESAASVerdict=POSITIVE ESAAVVerdict=NEGATIVE ESACFVerdict=MATCH endTime=Mon Aug 10 09:44:13 2020 ESADLPVerdict=NOT_EVALUATED dvc=1.1.1.1 ESAFriendlyFrom=sample_user ESAGMVerdict=NEGATIVE startTime=Mon Aug 10 09:44:13 2020 deviceInboundInterface=Outbound deviceDirection=0 ESAMailFlowPolicy=ACCEPT suser=dummy_suser cs1Label=MailPolicy cs1=DEFAULT cs2Label=SenderCountry cs2=unable to retrieve ESAMFVerdict=MATCH act=DELIVERED cs4Label=ExternalMsgID cs4=\'<sample@cs4>\' ESAOFVerdict=NEGATIVE duser=sample_duser ESAHeloIP=10.0.0.1 cfp1Label=SBRSScore cfp1=unable to retrieve sourceHostName=unknown ESASenderGroup=SUSPECTLIST sourceAddress=192.11.36.1 msg=\'test\''
+    '{{mark}} {{ bsd }} {{ host }} {{ app }}: Mon Aug 10 10:10:07 2020: CEF:0|Cisco|C100V Email Security Virtual Appliance|13.0.0-283|ESA_CONSOLIDATED_LOG_EVENT|Consolidated Log Event|5|cs6Label=SDRRepScore cs6=Weak deviceExternalId=111111111111-ZZZZZZZZZZZ ESAMID=19 startTime=Mon Aug 10 10:10:07 2020 deviceOutboundInterface=OutgoingMail ESADMARCVerdict=PermFailure dvc=1.1.1.1 ESAAttachmentDetails={\'sample_ESA_attachment\':   {\'AMP\': {\'Verdic\': \'FILE UNKNOWN\', \'fileHash\': \'35918c38cea9ad9279ad0b206091be3085874528edaf6994b73f0758679815a3\'},  \'BodyScanner\': {}}} ESAFriendlyFrom=sample_user deviceDirection=1 ESAMailFlowPolicy=ACCEPT suser=sample_user cs1Label=MailPolicy cs1=DEFAULT act=BOUNCED ESAFinalActionDetails=To SPAM cs4Label=ExternalMsgID cs4=\'<dummy@cs4>\' duser=dummy_duser ESAHeloIP=10.0.0.1 cfp1Label=SBRSScore cfp1=None ESASDRDomainAge=83 years 10 months 26 days cs3Label=SDRThreatCategory cs3=N/A ESASPFVerdict=PermError sourceHostName=unknown ESASenderGroup=SUSPECTLIST sourceAddress=5001:0db8:85a3:2222:6565:7457:0370:5453 ESAICID=30 cs5Label=ESAMsgLanguage cs5=English msg=[Cousin\=20Domain][SUSPECTED\=20SPAM]\=20asdfdsaf cs2Label=GeoLocation cs2=India ESAMsgTooBigFromSender=true ESARateLimitedIP=10.0.0.2 ESADHASource=10.0.0.3 ESAHeloDomain=test.com ESATLSOutConnStatus=Success ESATLSOutProtocol=TLSv1.2 ESATLSOutCipher=ECDHE-RSA-AES128-GCM-SHA256 ESATLSInConnStatus=Success ESATLSInProtocol=TLSv1.2 ESATLSInCipher=ECDHE-RSA-AES128-GCM-SHA256 ESADKIMVerdict=PermError ESAReplyTo=demo@test.com ESAASVerdict=SUSPECT ESAAMPVerdict=FA_PENDING ESAAVVerdict=REPAIRED ESAGMVerdict=POSITIVE ESACFVerdict=MATCH ESAOFVerdict=NEGATIVE ESADLPVerdict=NO VIOLATION ESAURLDetails={url1:{expanded_url: sample_expanded_url, category: dummy_category, wbrs_score: 62, in_attachment: sample_attachment_file, Attachment_with_url: www.sample.attachment.url.com,},url2:{…}} ESAMARAction= {action:success;succesful_rcpts=56;failed_recipients=0;filename=dummy_filename.txt} Message Filters Verdict=MATCH ESADCID=507 EndTime=Mon Aug 10 10:10:07 2020 ESADaneStatus=success ESADaneHost=testdomain.com',
+    '{{mark}} {{ bsd }} {{ host }} {{ app }}: Mon Aug 10 10:10:07 2020: CEF:0|Cisco|C190 Email Security Appliance|13.5.0-135|ESA_CONSOLIDATED_LOG_EVENT|Consolidated Log Event|5|deviceExternalId=111111111111-ZZZZZZZZZZZ ESAMID=99 ESAICID=27 ESAAMPVerdict=SKIPPED ESAASVerdict=POSITIVE ESAAVVerdict=NEGATIVE ESACFVerdict=MATCH endTime=Mon Aug 10 09:44:13 2020 ESADLPVerdict=NOT_EVALUATED dvc=1.1.1.1 ESAFriendlyFrom=sample_user ESAGMVerdict=NEGATIVE startTime=Mon Aug 10 09:44:13 2020 deviceInboundInterface=Outbound deviceDirection=0 ESAMailFlowPolicy=ACCEPT suser=dummy_suser cs1Label=MailPolicy cs1=DEFAULT cs2Label=SenderCountry cs2=unable to retrieve ESAMFVerdict=MATCH act=DELIVERED cs4Label=ExternalMsgID cs4=\'<sample@cs4>\' ESAOFVerdict=NEGATIVE duser=sample_duser ESAHeloIP=10.0.0.1 cfp1Label=SBRSScore cfp1=unable to retrieve sourceHostName=unknown ESASenderGroup=SUSPECTLIST sourceAddress=192.11.36.1 msg=\'test\''
 ];
 
 testdata_textmail = [
@@ -55,6 +55,7 @@ testdata_authentication = [
     '{{mark}} {{ bsd }} {{ host }} {{ app }}: Mon Aug 10 09:43:53 2020 Info: User dummy_user2 from 184.186.3.161 failed authentication.',
     '{{mark}} {{ bsd }} {{ host }} {{ app }}: Mon Aug 10 09:37:45 2020 Info: Version: 8.7.2-004 SN: 1024E857D276-JXKWBK2',
     '{{mark}} {{ bsd }} {{ host }} {{ app }}: Mon Aug 10 09:44:51 2020 Info: logout:64.205.160.240 user:dummy_user1 session:LH09MofqDf2j21zW9QN1',
+    '{{mark}} {{ bsd }} {{ host }} {{ app }}: Aug  3 07:26:33  10.0.1.1 MAR_SecurityAudit: Info: Message containing attachment(s) for which verdict update was(were) available was not found in the recipient\'s (<EMAIL>) mailbox.',
 ];
 
 @pytest.mark.parametrize("event", testdata_http)
@@ -195,33 +196,5 @@ def test_cisco_esa_authentication(record_property, setup_wordlist, setup_splunk,
     record_property("resultCount", resultCount)
     record_property("message", message)
 
-    assert resultCount == 8
+    assert resultCount == 9
 
-def test_cisco_esa_mar(record_property, setup_wordlist, setup_splunk, setup_sc4s):
-    host = "cisco_esa"
-
-    dt = datetime.datetime.now()
-    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
-
-    # Tune time functions
-    epoch = epoch[:-3]
-
-    mt = env.from_string(
-        '{{mark}} {{ bsd }} {{ host }} {{ app }}: Aug  3 07:26:33  10.0.1.1 MAR_SecurityAudit: Info: Message containing attachment(s) for which verdict update was(were) available was not found in the recipient\'s (<EMAIL>) mailbox.'
-    )
-    message = mt.render(mark="<111>", bsd=bsd, host=host, app='ESA')
-
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
-
-    st = env.from_string(
-        'search index=main _time={{ epoch }} sourcetype="cisco:esa:mar" host="{{ host }}"'
-    )
-    search = st.render(epoch=epoch, host=host)
-
-    resultCount, eventCount = splunk_single(setup_splunk, search)
-
-    record_property("host", host)
-    record_property("resultCount", resultCount)
-    record_property("message", message)
-
-    assert resultCount == 1
