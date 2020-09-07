0db561920f1d0957cb6c3fdd35d42faaf93b9308
rfaircloth-splunk <rfaircloth@splunk.com>
2020-09-07 09:02:36 -0400

    changes

--- deploy/byoe/sc4s.sh
+++ deploy/byoe/sc4s.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
-SC4S_ETC=${SC4S_ETC:=/opt/syslog-ng/etc}
-SC4S_TLS=${SC4S_ETC:=/opt/syslog-ng/tls}
-SC4S_VAR=${SC4S_VAR:=/opt/syslog-ng/var}
-SC4S_SBIN=${SC4S_SBIN:=/opt/syslog-ng/sbin}
-SC4S_BIN=${SC4S_SBIN:=/opt/syslog-ng/bin}
-SC4S_SBIN=${SC4S_SBIN:=/opt/syslog-ng/sbin}
+
 # The follwoing will be addressed in a future release
 # source scl_source enable rh-python36
 
@@ -28,8 +23,7 @@ if [ ${SC4S_LISTEN_CISCO_ASA_LEGACY_TLS_PORT} ]; then export SC4S_LISTEN_CISCO_A
 if [ ${SC4S_ARCHIVE_CISCO_ASA_LEGACY} ]; then export SC4S_ARCHIVE_CISCO_ASA=$SC4S_ARCHIVE_CISCO_ASA_LEGACY; fi
 if [ ${SC4S_DEST_CISCO_ASA_LEGACY_HEC} ]; then export SC4S_DEST_CISCO_ASA_HEC=$SC4S_DEST_CISCO_ASA_LEGACY_HEC; fi
 
-cd $SC4S_ETC
-mkdir -p local_config ||  true
+cd /etc
 
 # SIGTERM-handler
 term_handler() {
@@ -52,65 +46,65 @@ hup_handler() {
 trap 'kill ${!}; hup_handler' SIGHUP
 trap 'kill ${!}; term_handler' SIGTERM
 
-mkdir -p $SC4S_ETC/conf.d/local/context/
-mkdir -p $SC4S_ETC/conf.d/merged/context/
-mkdir -p $SC4S_ETC/conf.d/local/config/
+mkdir -p /etc/conf.d/local/context/
+mkdir -p /etc/conf.d/merged/context/
+mkdir -p /etc/conf.d/local/config/
 
 
 
-cp $SC4S_ETC/context_templates/* $SC4S_ETC/conf.d/local/context
-for file in $SC4S_ETC/conf.d/local/context/*.example ; do cp --verbose -n $file ${file%.example}; done
+cp /etc/context_templates/* /etc/conf.d/local/context
+for file in /etc/conf.d/local/context/*.example ; do cp --verbose -n $file ${file%.example}; done
 if [ "$SC4S_RUNTIME_ENV" == "k8s" ]
 then
-  mkdir -p $SC4S_ETC/conf.d/configmap/context/
-  mkdir -p $SC4S_ETC/conf.d/configmap/config/
+  mkdir -p /etc/conf.d/configmap/context/
+  mkdir -p /etc/conf.d/configmap/config/
   # Add new entries
   temp_file=$(mktemp)
-  awk '{print $0}' $SC4S_ETC/conf.d/configmap/context/splunk_metadata.csv $SC4S_ETC/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
-  cp -f $temp_file $SC4S_ETC/conf.d/merged/context/splunk_metadata.csv
+  awk '{print $0}' /etc/conf.d/configmap/context/splunk_metadata.csv /etc/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
+  cp -f $temp_file /etc/conf.d/merged/context/splunk_metadata.csv
 
 else
   # splunk_index.csv updates
   # Remove comment headers from existing config
-  touch $SC4S_ETC/conf.d/local/context/splunk_metadata.csv
-  if [ -f $SC4S_ETC/conf.d/local/context/splunk_index.csv ]; then
-      LEGACY_SPLUNK_INDEX_FILE=$SC4S_ETC/conf.d/local/context/splunk_index.csv
+  touch /etc/conf.d/local/context/splunk_metadata.csv
+  if [ -f /etc/conf.d/local/context/splunk_index.csv ]; then
+      LEGACY_SPLUNK_INDEX_FILE=/etc/conf.d/local/context/splunk_index.csv
   fi
 
   # Add new entries
   temp_file=$(mktemp)
-  awk '{print $0}' ${LEGACY_SPLUNK_INDEX_FILE} $SC4S_ETC/conf.d/local/context/splunk_metadata.csv $SC4S_ETC/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
-  cp -f $temp_file $SC4S_ETC/conf.d/merged/context/splunk_metadata.csv
+  awk '{print $0}' ${LEGACY_SPLUNK_INDEX_FILE} /etc/conf.d/local/context/splunk_metadata.csv /etc/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
+  cp -f $temp_file /etc/conf.d/merged/context/splunk_metadata.csv
   # We don't need this file any longer
-  rm -f $SC4S_ETC/conf.d/local/context/splunk_index.csv.example || true 
-  if [ -f $SC4S_ETC/conf.d/local/context/splunk_index.csv ]; then
-      cp -f $SC4S_ETC/conf.d/local/context/splunk_index.csv $SC4S_ETC/conf.d/local/context/splunk_index.deprecated
-      rm $SC4S_ETC/conf.d/local/context/splunk_index.csv
+  rm -f /etc/conf.d/local/context/splunk_index.csv.example || true 
+  if [ -f /etc/conf.d/local/context/splunk_index.csv ]; then
+      cp -f /etc/conf.d/local/context/splunk_index.csv /etc/conf.d/local/context/splunk_index.deprecated
+      rm /etc/conf.d/local/context/splunk_index.csv
   fi
-  cp --verbose -R -f $SC4S_ETC/local_config/* $SC4S_ETC/conf.d/local/config/
+  cp --verbose -R -f /etc/local_config/* /etc/conf.d/local/config/
 fi  
-mkdir -p $SC4S_VAR/log
+mkdir -p /var/log
 
 # Test HEC Connectivity
 if [ "$SC4S_DEST_SPLUNK_HEC_GLOBAL" != "no" ]
 then
   HEC=$(echo '{{- getenv "SPLUNK_HEC_URL" | strings.ReplaceAll "/services/collector" "" | strings.ReplaceAll "/event" "" | regexp.ReplaceLiteral "[, ]+" "/services/collector/event " }}/services/collector/event' | gomplate | cut -d' ' -f 1)
   NO_VERIFY=$(echo '{{- if not (conv.ToBool (getenv "SC4S_DEST_SPLUNK_HEC_TLS_VERIFY" "yes")) }}-k{{- end}}' | gomplate)
-  SC4S_DEST_SPLUNK_HEC_FALLBACK_INDEX=$(cat $SC4S_ETC/conf.d/local/context/splunk_metadata.csv | grep ',index,' | grep sc4s_events | cut -d, -f 3)
+  SC4S_DEST_SPLUNK_HEC_FALLBACK_INDEX=$(cat /etc/conf.d/local/context/splunk_metadata.csv | grep ',index,' | grep sc4s_events | cut -d, -f 3)
   export SC4S_DEST_SPLUNK_HEC_FALLBACK_INDEX
   if curl -s -S ${NO_VERIFY} "${HEC}?/index=${SC4S_DEST_SPLUNK_HEC_FALLBACK_INDEX}" -H "Authorization: Splunk ${SPLUNK_HEC_TOKEN}" -d '{"event": "HEC TEST EVENT", "sourcetype": "SC4S:PROBE"}' 2>&1 | grep -v '{"text":"Success","code":0}'
   then
     echo -e "SC4S_ENV_CHECK_HEC: Invalid Splunk HEC URL, invalid token, or other HEC connectivity issue.\nStartup will continue to prevent data loss if this is a transient failure."
   else
     echo -e "\nSC4S_ENV_CHECK_HEC: Splunk HEC connection test successful; checking indexes...\n"
-    cat $SC4S_ETC/conf.d/local/context/splunk_metadata.csv  | grep -v sc4s_metrics | grep ',index,' | cut -d, -f 3 | sort -u | while read index ; do export index; echo -e "SC4S_ENV_CHECK_INDEX: Checking $index" $(curl -s -S -k "${HEC}?index=${index}" -H "Authorization: Splunk ${SPLUNK_HEC_TOKEN}" -d '{"event": "HEC TEST EVENT", "sourcetype": "SC4S:PROBE"}') ; done
+    cat /etc/conf.d/local/context/splunk_metadata.csv  | grep -v sc4s_metrics | grep ',index,' | cut -d, -f 3 | sort -u | while read index ; do export index; echo -e "SC4S_ENV_CHECK_INDEX: Checking $index" $(curl -s -S -k "${HEC}?index=${index}" -H "Authorization: Splunk ${SPLUNK_HEC_TOKEN}" -d '{"event": "HEC TEST EVENT", "sourcetype": "SC4S:PROBE"}') ; done
   fi
 fi
 
 # Run gomplate to create config from templates if the command errors this is fatal
 # Stop the container. Errors in this step should only happen with user provided 
 # Templates
-if ! gomplate $(find . -name *.tmpl | sed -E 's/^(\/.*\/)*(.*)\..*$/--file=\2.tmpl --out=\2/') --template t=$SC4S_ETC/go_templates/; then
+if ! gomplate $(find . -name *.tmpl | sed -E 's/^(\/.*\/)*(.*)\..*$/--file=\2.tmpl --out=\2/') --template t=etc/go_templates/; then
   echo "Error in Gomplate template; unable to continue, exiting..."
   exit 800
 fi
@@ -119,13 +113,13 @@ fi
 
 if [ "$SC4S_SNMP_TRAP_COLLECT" == "yes" ]
 then
-/opt/net-snmp/sbin/snmptrapd -Lf $SC4S_VAR/log/snmptrapd.log
+/opt/net-snmp/sbin/snmptrapd -Lf /var/log/snmptrapd.log
 fi
 
 echo syslog-ng checking config
 echo sc4s version=$(cat /VERSION)
-echo sc4s version=$(cat /VERSION) >$SC4S_VAR/log/syslog-ng.out
-$SC4S_SBIN/syslog-ng -s >>$SC4S_VAR/log/syslog-ng.out 2>$SC4S_VAR/log/syslog-ng.err
+echo sc4s version=$(cat /VERSION) >/var/log/syslog-ng.out
+/sbin/syslog-ng -s >>/var/log/syslog-ng.out 2>/var/log/syslog-ng.err
 
 # Use gomplate to pick up default listening ports for health check
 echo starting goss
@@ -133,9 +127,9 @@ gomplate --file /goss.yaml.tmpl --out /goss.yaml
 goss -g /goss.yaml serve --format json >/dev/null 2>/dev/null &
 
 echo syslog-ng starting
-$SC4S_BIN/bin/persist-tool add $SC4S_ETC/reset_persist -o $SC4S_VAR
+/bin/persist-tool add /etc/reset_persist -o /var
 
-$SC4S_SBIN/syslog-ng $@ &
+/sbin/syslog-ng $@ &
 pid="$!"
 sleep 5
 if ! ps -p $pid > /dev/null
