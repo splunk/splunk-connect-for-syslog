851f109711a7cbe298ec3b98e93bdaee86fb989e
Mahir Chavda <mchavda@splunk.com>
2020-09-28 12:21:57 +0000

    Add DNS and DHCP sample event

--- tests/test_infoblox.py
+++ tests/test_infoblox.py
@@ -6,6 +6,7 @@
 import datetime
 import random
 import pytz
+import pytest
 
 from jinja2 import Environment, environment
 
@@ -15,9 +16,37 @@ from .timeutils import *
 
 env = Environment()
 
-#<142>Oct 25 13:08:00 161.231.218.156 named[6597]: FORMERR resolving 'www.google.com/AAAA/IN': 209.2.230.6#53
-def test_infoblox_dns(record_property, setup_wordlist, setup_splunk, setup_sc4s):
-    host = "vib-{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+infoblox_dns_testdata = [
+    r'{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: Recursion cache view "_default": size = 89496, hits = 1, misses = 3',
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: 24-Sep-2020 09:46:27.205 client 192.168.1.3#61567: UDP: query: 2.1.168.192.in-addr.arpa IN PTR response: NXDOMAIN +",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: 24-Sep-2020 09:46:27.329 client 192.168.1.3#61568: UDP: query: abc.com IN A response: NOERROR + abc.com. 60 IN A 13.32.86.14; abc.com. 60 IN A 13.32.86.89; abc.com. 60 IN A 13.32.86.7; abc.com. 60 IN A 13.32.86.42;",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: 24-Sep-2020 09:46:27.336 client 192.168.1.3#61569: UDP: query: abc.com IN AAAA response: NOERROR +",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: client @0x7f0c74087120 192.168.1.3#61568 (abc.com): query: abc.com IN A + (192.168.1.2)",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: client @0x7f0c840cc860 192.168.1.3#61567 (2.1.168.192.in-addr.arpa): query: 2.1.168.192.in-addr.arpa IN PTR + (192.168.1.2)",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: client @0x7f0c840cc860 192.168.1.3#61567 (2.1.168.192.in-addr.arpa): RFC 1918 response from Internet for 2.1.168.192.in-addr.arpa",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: client @0x7f0c840cc860 192.168.1.3#61569 (abc.com): query: abc.com IN AAAA + (192.168.1.2)",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: FORMERR resolving 'www.google.com/AAAA/IN': 209.2.230.6#53",
+    r"{{ mark }}{{ bsd }} {{ host }} named[{{ pid }}]: Recursion client quota: used/max/soft-limit/s-over/hard-limit/h-over/low-pri = 0/1/900/0/1000/0/0",
+]
+
+infoblox_dhcp_testdata = [
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: Abandoning IP address 192.168.1.125: pinged before offer",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPACK on 192.168.1.120 to 00:50:56:13:60:56 (dummyhost) via eth1 relay eth1 lease-duration 600 uid 01:00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPDISCOVER from 00:50:56:13:60:56 via eth1 TransID c02c6bb8 uid 01:00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPEXPIRE on 192.168.1.125 to 00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPOFFER on 192.168.1.120 to 00:50:56:13:60:56 (dummyhost) via eth1 relay eth1 lease-duration 119 offered-duration 600 uid 01:00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPRELEASE of 192.168.1.126 from 00:50:56:13:60:56 (dummyhost) via eth1 (found) TransID 8554a358 uid 01:00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPREQUEST for 10.130.151.62 from 80:ce:62:9c:0e:70 (DTCCE0826E00C97) via eth2 TransID 802c562c uid 01:80:ce:62:9c:0e:70 (RENEW)",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPREQUEST for 192.168.1.120 (192.168.1.2) from 00:50:56:13:60:56 (dummyhost) via eth1 TransID 9a5fbd6e uid 01:00:50:56:13:60:56",
+    r"{{ mark }}{{ bsd }} {{ host }} dhcpd[{{ pid }}]: uid lease 192.168.1.125 for client 00:50:56:13:60:56 is duplicate on 192.168.1.0/24",
+]
+
+@pytest.mark.parametrize("event", infoblox_dns_testdata)
+def test_infoblox_dns(record_property, setup_wordlist, setup_splunk, setup_sc4s, event):
+    host = "infoblox-{}-{}".format(
+        random.choice(setup_wordlist), random.choice(setup_wordlist)
+    )
     pid = random.randint(1000, 32000)
 
     dt = datetime.datetime.now()
@@ -26,12 +55,14 @@ def test_infoblox_dns(record_property, setup_wordlist, setup_splunk, setup_sc4s)
     # Tune time functions
     epoch = epoch[:-7]
 
-    mt = env.from_string("{{ mark }} {{ bsd }} {{ host }} named[{{ pid }}]: FORMERR resolving 'www.google.com/AAAA/IN': 209.2.230.6#53\n")
+    mt = env.from_string(event + "\n")
     message = mt.render(mark="<111>", bsd=bsd, host=host, pid=pid)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
-    st = env.from_string("search _time={{ epoch }} index=netdns host={{ host }} sourcetype=\"infoblox:dns\"")
+    st = env.from_string(
+        'search _time={{ epoch }} index=netdns host={{ host }} sourcetype="infoblox:dns"'
+    )
     search = st.render(epoch=epoch, host=host)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
@@ -42,9 +73,14 @@ def test_infoblox_dns(record_property, setup_wordlist, setup_splunk, setup_sc4s)
 
     assert resultCount == 1
 
-#<150>Oct 24 21:09:00 162.101.157.246 dhcpd[28922]: DHCPREQUEST for 10.130.151.62 from 80:ce:62:9c:0e:70 (DTCCE0826E00C97) via eth2 TransID 802c562c uid 01:80:ce:62:9c:0e:70 (RENEW)
-def test_infoblox_dhcp(record_property, setup_wordlist, setup_splunk, setup_sc4s):
-    host = "vib-{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+@pytest.mark.parametrize("event", infoblox_dhcp_testdata)
+def test_infoblox_dhcp(
+    record_property, setup_wordlist, setup_splunk, setup_sc4s, event
+):
+    host = "infoblox-{}-{}".format(
+        random.choice(setup_wordlist), random.choice(setup_wordlist)
+    )
     pid = random.randint(1000, 32000)
 
     dt = datetime.datetime.now()
@@ -53,12 +89,14 @@ def test_infoblox_dhcp(record_property, setup_wordlist, setup_splunk, setup_sc4s
     # Tune time functions
     epoch = epoch[:-7]
 
-    mt = env.from_string("{{ mark }} {{ bsd }} {{ host }} dhcpd[{{ pid }}]: DHCPREQUEST for 10.00.00.62 from 80:00:00:00:0e:70 (EXAMPLE) via eth2 TransID 802c562c uid 01:80:00:00:00:00:70 (RENEW)\n")
+    mt = env.from_string(event + "\n")
     message = mt.render(mark="<150>", bsd=bsd, host=host, pid=pid)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
-    st = env.from_string("search _time={{ epoch }} index=netipam host={{ host }} sourcetype=\"infoblox:dhcp\"")
+    st = env.from_string(
+        'search _time={{ epoch }} index=netipam host={{ host }} sourcetype="infoblox:dhcp"'
+    )
     search = st.render(epoch=epoch, host=host)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
