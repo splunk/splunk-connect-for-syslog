4ece5b108d4d769df5e88064054d0df2d0935c8b
rfaircloth-splunk <rfaircloth@splunk.com>
2020-10-10 17:52:41 -0400

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -197,6 +197,7 @@ def sc4s_docker(docker_services):
     ports = {514:  docker_services.port_for("sc4s", 514)}
     ports = {601:  docker_services.port_for("sc4s", 601)}
 
+
     for x in range(5000, 5015):
         ports.update({x: docker_services.port_for("sc4s", x)})
 
