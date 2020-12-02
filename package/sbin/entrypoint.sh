03fd1f08be7ba02352104c4ece490b4a1e2ab298
rfaircloth-splunk <rfaircloth@splunk.com>
2020-12-02 16:36:42 -0500

    Update entrypoint.sh

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
+​
 # These path variables allow for a single entrypoint script to be utilized for both Container and BYOE runtimes
 export SC4S_ETC=${SC4S_ETC:=/etc/syslog-ng}
 export SC4S_TLS=${SC4S_TLS:=/etc/syslog-ng/tls}
 export SC4S_VAR=${SC4S_VAR:=/var/lib/syslog-ng}
 export SC4S_BIN=${SC4S_BIN:=/usr/bin}
 export SC4S_SBIN=${SC4S_SBIN:=/usr/sbin}
+​
 # The follwoing will be addressed in a future release
 # source scl_source enable rh-python36
+​
 # The MICROFOCUS_ARCSIGHT destination is currently deprecated
 # The unique port environment variables associated with MICROFOCUS_ARCSIGHT will be renamed to
 # match the current CEF destination
@@ -16,6 +19,7 @@ if [ ${SC4S_LISTEN_MICROFOCUS_ARCSIGHT_TCP_PORT} ]; then export SC4S_LISTEN_CEF_
 if [ ${SC4S_LISTEN_MICROFOCUS_ARCSIGHT_TLS_PORT} ]; then export SC4S_LISTEN_CEF_TLS_PORT=$SC4S_LISTEN_MICROFOCUS_ARCSIGHT_TLS_PORT; fi
 if [ ${SC4S_ARCHIVE_MICROFOCUS_ARCSIGHT} ]; then export SC4S_ARCHIVE_CEF=$SC4S_ARCHIVE_MICROFOCUS_ARCSIGHT; fi
 if [ ${SC4S_DEST_MICROFOCUS_ARCSIGHT_HEC} ]; then export SC4S_DEST_CEF_HEC=$SC4S_DEST_MICROFOCUS_ARCSIGHT_HEC; fi
+​
 # The CISCO_ASA_LEGACY destination is currently deprecated
 # The unique port environment variables associated with CISCO_ASA_LEGACY will be renamed to
 # match the current CISCO_ASA destination
@@ -25,6 +29,7 @@ if [ ${SC4S_LISTEN_CISCO_ASA_LEGACY_TCP_PORT} ]; then export SC4S_LISTEN_CISCO_A
 if [ ${SC4S_LISTEN_CISCO_ASA_LEGACY_TLS_PORT} ]; then export SC4S_LISTEN_CISCO_ASA_TLS_PORT=$SC4S_LISTEN_CISCO_ASA_LEGACY_TLS_PORT; fi
 if [ ${SC4S_ARCHIVE_CISCO_ASA_LEGACY} ]; then export SC4S_ARCHIVE_CISCO_ASA=$SC4S_ARCHIVE_CISCO_ASA_LEGACY; fi
 if [ ${SC4S_DEST_CISCO_ASA_LEGACY_HEC} ]; then export SC4S_DEST_CISCO_ASA_HEC=$SC4S_DEST_CISCO_ASA_LEGACY_HEC; fi
+​
 # SIGTERM-handler
 term_handler() {
 # SIGTERM on valid PID; return exit code 0 (clean exit)
@@ -37,6 +42,7 @@ term_handler() {
 # 128 + 15 -- SIGTERM on non-existent process (will cause service failure)
   exit 143
 }
+​
 # SIGHUP-handler
 hup_handler() {
   if [ $pid -ne 0 ]; then
@@ -44,6 +50,7 @@ hup_handler() {
     kill -SIGHUP ${pid}
   fi
 }
+​
 # SIGQUIT-handler
 quit_handler() {
   if [ $pid -ne 0 ]; then
@@ -52,9 +59,11 @@ quit_handler() {
     wait ${pid}
   fi
 }
+​
 trap 'kill ${!}; hup_handler' SIGHUP
 trap 'kill ${!}; term_handler' SIGTERM
 trap 'kill ${!}; quit_handler' SIGQUIT
+​
 if [ "$SC4S_MIGRATE_CONFIG" == "yes" ]
 then
   if [ -d /opt/syslog-ng/var ]; then
@@ -75,11 +84,13 @@ then
     ln -s /opt/syslog-ng/tls /etc/syslog-ng/tls
   fi
 fi
+​
 mkdir -p $SC4S_VAR/log/
 mkdir -p $SC4S_ETC/conf.d/local/context/
 mkdir -p $SC4S_ETC/conf.d/merged/context/
 mkdir -p $SC4S_ETC/conf.d/local/config/
 mkdir -p $SC4S_ETC/local_config/
+​
 cp -f $SC4S_ETC/context_templates/* $SC4S_ETC/conf.d/local/context
 for file in $SC4S_ETC/conf.d/local/context/*.example ; do cp --verbose -n $file ${file%.example}; done
 if [ "$SC4S_RUNTIME_ENV" == "k8s" ]
@@ -90,6 +101,7 @@ then
   temp_file=$(mktemp)
   awk '{print $0}' $SC4S_ETC/conf.d/configmap/context/splunk_metadata.csv $SC4S_ETC/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
   cp -f $temp_file $SC4S_ETC/conf.d/merged/context/splunk_metadata.csv
+​
 else
   # splunk_index.csv updates
   # Remove comment headers from existing config
@@ -97,6 +109,7 @@ else
   if [ -f $SC4S_ETC/conf.d/local/context/splunk_index.csv ]; then
       LEGACY_SPLUNK_INDEX_FILE=$SC4S_ETC/conf.d/local/context/splunk_index.csv
   fi
+​
   # Add new entries
   temp_file=$(mktemp)
   awk '{print $0}' ${LEGACY_SPLUNK_INDEX_FILE} $SC4S_ETC/conf.d/local/context/splunk_metadata.csv $SC4S_ETC/context_templates/splunk_metadata.csv.example | grep -v '^#' | sort -b -t ',' -k1,2 -u  > $temp_file
@@ -109,6 +122,7 @@ else
   fi
   cp --verbose -R -f $SC4S_ETC/local_config/* $SC4S_ETC/conf.d/local/config/
 fi
+​
 # Test HEC Connectivity
 SPLUNK_HEC_URL=$(echo $SPLUNK_HEC_URL | sed 's/\(https\{0,1\}\:\/\/[^\/, ]*\)[^, ]*/\1\/services\/collector\/event/g' | sed 's/,/ /g')
 if [ "$SC4S_DEST_SPLUNK_HEC_GLOBAL" != "no" ]
@@ -125,6 +139,7 @@ then
     cat $SC4S_ETC/conf.d/merged/context/splunk_metadata.csv  | grep -v sc4s_metrics | grep ',index,' | cut -d, -f 3 | sort -u | while read index ; do export index; echo -e "SC4S_ENV_CHECK_INDEX: Checking $index" $(curl -s -S -k "${HEC}?index=${index}" -H "Authorization: Splunk ${SPLUNK_HEC_TOKEN}" -d '{"event": "HEC TEST EVENT", "sourcetype": "SC4S:PROBE"}') ; done
   fi
 fi
+​
 # Create a workable variable with a list of simple log paths
 export SOURCE_SIMPLE_SET=$(printenv | grep '^SC4S_LISTEN_SIMPLE_.*_PORT' | sed 's/^SC4S_LISTEN_SIMPLE_//;s/_..._PORT\=.*//;s/_...._PORT\=.*//' | sort | uniq |  xargs echo | sed 's/ /,/g' | tr '[:upper:]' '[:lower:]' )
 # Run gomplate to create config from templates if the command errors this is fatal
@@ -135,10 +150,12 @@ export SOURCE_PLUGINS_RFC5424=$(ls sp_rfc5424_*.t -1p | xargs echo | sed 's/ /,/
 export SOURCE_PLUGINS_NS=$(ls sp_ns_*.t -1p | xargs echo | sed 's/ /,/g')
 export SOURCE_PLUGINS_RFC3164=$(ls sp_rfc3164_*.t -1p | xargs echo | sed 's/ /,/g')
 popd
+​
 if ! gomplate $(find . -name "*.tmpl" | sed -E 's/^(\/.*\/)*(.*)\..*$/--file=\2.tmpl --out=\2/') --template t=$SC4S_ETC/go_templates/; then
   echo "Error in Gomplate template; unable to continue, exiting..."
   exit 800
 fi
+​
 # OPTIONAL for BYOE:  Comment out SNMP stanza immediately below and launch snmptrapd directly from systemd
 # Launch snmptrapd
 #
