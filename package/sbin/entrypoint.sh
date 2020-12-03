9024f767b90182e7223d584ca5696843ccae6c16
mbonsack <mbonsack@splunk.com>
2020-12-03 04:26:19 -0800

    fix: Improve config path migration script logic

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -30,9 +30,6 @@ if [ ${SC4S_LISTEN_CISCO_ASA_LEGACY_TLS_PORT} ]; then export SC4S_LISTEN_CISCO_A
 if [ ${SC4S_ARCHIVE_CISCO_ASA_LEGACY} ]; then export SC4S_ARCHIVE_CISCO_ASA=$SC4S_ARCHIVE_CISCO_ASA_LEGACY; fi
 if [ ${SC4S_DEST_CISCO_ASA_LEGACY_HEC} ]; then export SC4S_DEST_CISCO_ASA_HEC=$SC4S_DEST_CISCO_ASA_LEGACY_HEC; fi
 
-cd $SC4S_ETC
-mkdir -p local_config
-
 # SIGTERM-handler
 term_handler() {
 # SIGTERM on valid PID; return exit code 0 (clean exit)
@@ -82,7 +79,6 @@ then
   if [ -d /opt/syslog-ng/tls ]; then
     ln -s /opt/syslog-ng/tls /etc/syslog-ng/tls
   fi
-
 fi
 
 mkdir -p $SC4S_VAR/log/
