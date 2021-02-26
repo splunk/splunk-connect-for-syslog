d76a4dcb26ac0f83b961451965247dbb0fd2e074
Addon Factory template <rfaircloth@splunk.com>
2021-02-26 11:38:42 -0500

    update mcafee
    fix(mcafee_wg): Correct vendor product by key should be mcafee_wg not mcafeewg
    feat(app-parser): Use app-parser for Mcafee web gateway

--- tests/test_mcafee_web_gateway.py
+++ tests/test_mcafee_web_gateway.py
@@ -31,7 +31,7 @@ def test_data_mcafeewg(record_property, setup_wordlist, setup_splunk, setup_sc4s
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
     st = env.from_string(
-        'search index=main _time={{ epoch }} sourcetype="mcafee:wg:kv" source="mcafee:wg" host="{{ host }}" _raw="{{ message }}"'
+        'search index=netproxy _time={{ epoch }} sourcetype="mcafee:wg:kv" source="mcafee:wg" host="{{ host }}" _raw="{{ message }}"'
     )
     
     message1='mwg: status=\\"403/80\\" srcip=\\"192.168.57.1\\" user=\\"-\\" dhost=\\"s3-eu-west-1.amazonaws.com\\" urlp=\\"80\\" proto=\\"HTTP/http\\" mtd=\\"GET\\" urlc=\\"Internet Services\\" rep=\\"-31\\" mt=\\"application/pdf\\" mlwr=\\"BehavesLike.PDF.Exploit.vx\\" app=\\"-\\" bytes=\\"458/509/2813889/3001\\" ua=\\"Chrome87-10.0\\" http_referrer=\\"http://www.cpcheckme.com/\\" lat=\\"0/0/165/2638\\" rule=\\"Block If Virus Was Found\\" url=\\"http://s3-eu-west-1.amazonaws.com/cp-chk-files/win7_64bit_big.pdf?static=CPCheckMe&rand=1608029225651\\" file_name=\\"win7_64bit_big.pdf\\" destip=\\"52.218.52.116\\" rep_level=\\"Minimal Risk\\"'
