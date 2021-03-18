9e4107a5d82a2ef496bf78fd0aa3b1eafd6d0d62
Addon Factory template <rfaircloth@splunk.com>
2021-03-18 09:40:14 -0400

    test: Reduce ports consumed to improve test time

--- tests/test_pfsense.py
+++ tests/test_pfsense.py
@@ -26,7 +26,7 @@ def test_pfsense_filterlog(record_property, setup_wordlist, setup_splunk, setup_
     mt = env.from_string(
         "{{mark}}{{ bsd }} filterlog: 82,,,1000002666,mvneta2,match,pass,out,6,0x00,0x00000,64,ICMPv6,58,8,{{key}},\n")
     message = mt.render(mark="<27>", bsd=bsd, key=host)
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][5006])
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][6000])
 
     st = env.from_string("search _time={{ epoch }} index=netfw sourcetype=pfsense:filterlog \"{{key}}\"")
     search = st.render(epoch=epoch, key=host)
@@ -53,7 +53,7 @@ def test_pfsense_other(record_property, setup_wordlist, setup_splunk, setup_sc4s
     mt = env.from_string(
         "{{mark}}{{ bsd }} kqueue error: {{key}}\n")
     message = mt.render(mark="<27>", bsd=bsd, key=host)
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][5006])
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][6000])
 
     st = env.from_string("search _time={{ epoch }} index=netops sourcetype=pfsense:* \"{{key}}\"")
     search = st.render(epoch=epoch, key=host)
@@ -79,7 +79,7 @@ def test_pfsense_syslogd(record_property, setup_wordlist, setup_splunk, setup_sc
     mt = env.from_string(
         "{{mark}}{{ bsd }} syslogd: restart {{key}}\n")
     message = mt.render(mark="<27>", bsd=bsd, key=host)
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][5006])
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][6000])
 
     st = env.from_string("search _time={{ epoch }} index=netops sourcetype=pfsense:syslogd \"{{key}}\"")
     search = st.render(epoch=epoch, key=host)
