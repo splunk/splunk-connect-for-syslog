368c58040b293f2fedb9565503e342506ee01631
rfaircloth-splunk <rfaircloth@splunk.com>
2021-08-04 14:50:18 -0400

    fix: bump

--- .github/release.sh
+++ .github/release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 echo publish $1 $2
-
-docker pull ghcr.io/splunk-connect-for-syslog:splunk-connect-for-syslog:$1
-docker save ghcr.io/splunk-connect-for-syslog:splunk-connect-for-syslog:$1 | gzip -c > /tmp/workspace/oci_container.tar.gz
+docker pull ghcr.io/splunk/splunk-connect-for-syslog:splunk-connect-for-syslog:$1
+docker save ghcr.io/splunk/splunk-connect-for-syslog:splunk-connect-for-syslog:$1 | gzip -c > /tmp/workspace/oci_container.tar.gz
