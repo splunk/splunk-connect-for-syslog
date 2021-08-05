a2054e9ce87d3395de7dc51f338925281cc8fc11
Ryan Faircloth <rfaircloth@splunk.com>
2021-08-05 11:20:22 -0400

    fix: Bump release to test CI migration

---
+++ .github/release.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo publish $1 $2
+docker pull ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog:$1
+docker save ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog:$1 | gzip -c > /tmp/oci_container.tar.gz
