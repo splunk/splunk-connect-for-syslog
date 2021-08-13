9b06770ca47d13744ee1eb6c8c6423822cc1f740
rfaircloth-splunk <rfaircloth@splunk.com>
2021-08-13 11:33:39 -0400

    fix: Improve release process

--- .github/release.sh
+++ .github/release.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+set -e
 
 echo publish $1 $2
 docker pull ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog:$1
 docker save ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog:$1 | gzip -c > /tmp/oci_container.tar.gz
+
+tags=$(echo $CONTAINER_SOURCE_IMAGE |sed 's|ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog|docker.io/splunk/scs|g')
+/tmp/regctl image copy $CONTAINER_SOURCE_IMAGE docker.io/splunk/scs:$1
+for line in $tags; do echo working on "$line"; /tmp/regctl image copy $CONTAINER_SOURCE_IMAGE $line; done
