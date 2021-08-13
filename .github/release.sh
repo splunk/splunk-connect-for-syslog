8404e0f4c9a0afbc60f748ce693eee9e6de52c18
rfaircloth-splunk <rfaircloth@splunk.com>
2021-08-13 12:16:26 -0400

    fix: retag after first copy don't copy again

--- .github/release.sh
+++ .github/release.sh
@@ -7,4 +7,4 @@ docker save ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog:$
 
 tags=$(echo $CONTAINER_SOURCE_IMAGE |sed 's|ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog|docker.io/splunk/scs|g')
 /tmp/regctl image copy $CONTAINER_SOURCE_IMAGE docker.io/splunk/scs:$1
-for line in $tags; do echo working on "$line"; /tmp/regctl image copy $CONTAINER_SOURCE_IMAGE $line; done
+for line in $tags; do echo working on "$line"; /tmp/regctl image copy docker.io/splunk/scs:$1 $line; done
