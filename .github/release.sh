0191614759eaa5bac78cda6498ec6a88eb943268
rfaircloth-splunk <rfaircloth@splunk.com>
2021-08-19 08:50:36 -0400

    Revert "fix: revert to old package"
    This reverts commit 796b3d1468bc0407ef36c3a7efdecefbe8cb5085.

--- .github/release.sh
+++ .github/release.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo publish $1 $2
-tags=$(echo $CONTAINER_SOURCE_IMAGE |sed 's|ghcr.io/splunk/splunk-connect-for-syslog/splunk-connect-for-syslog|docker.io/splunk/scs|g')
+tags=$(echo $CONTAINER_SOURCE_IMAGE |sed 's|ghcr.io/splunk/splunk-connect-for-syslog/container|docker.io/splunk/scs|g')
 /tmp/regctl image copy $CONTAINER_SOURCE_IMAGE docker.io/splunk/scs:$1
 for line in $tags; do echo working on "$line"; /tmp/regctl image copy docker.io/splunk/scs:$1 $line; done
