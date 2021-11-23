0272f4f1f93746bec482bdb4e7aa36926aa1dd4d
rfaircloth-splunk <rfaircloth@splunk.com>
2021-11-23 07:19:26 -0500

    test: fix meraki test syntax

--- tests/test_cisco_meraki.py
+++ tests/test_cisco_meraki.py
@@ -17,7 +17,9 @@ env = Environment()
 def test_cisco_meraki_security_event(
     record_property, setup_wordlist, setup_splunk, setup_sc4s
 ):
-    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+    host = "testcm-{}-{}".format(
+        random.choice(setup_wordlist), random.choice(setup_wordlist)
+    )
 
     dt = datetime.datetime.now()
     iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
@@ -26,14 +28,14 @@ def test_cisco_meraki_security_event(
     epoch_ms = epoch[:-3]
 
     mt = env.from_string(
-        "{{ mark }}1 {{ epoch }}123 testcm-{{ host }} security_event ids_alerted signature=1:28423:1 priority=1 timestamp={{ epoch }} dhost=98:5A:EB:E1:81:2F direction=ingress protocol=tcp/ip src=151.101.52.238:80 dst=192.168.128.2:53023 message: EXPLOIT-KIT Multiple exploit kit single digit exe detection\n"
+        "{{ mark }}1 {{ epoch }}123 {{ host }} security_event ids_alerted signature=1:28423:1 priority=1 timestamp={{ epoch }} dhost=98:5A:EB:E1:81:2F direction=ingress protocol=tcp/ip src=151.101.52.238:80 dst=192.168.128.2:53023 message: EXPLOIT-KIT Multiple exploit kit single digit exe detection\n"
     )
     message = mt.render(mark="<134>", epoch=epoch, host=host)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
     st = env.from_string(
-        'search _time={{ epoch_ms }} index=netfw host="testcm-{{ host }}" sourcetype="meraki"'
+        'search _time={{ epoch_ms }} index=netfw host="{{ host }}" sourcetype="meraki"'
     )
     search = st.render(epoch_ms=epoch_ms, host=host)
 
