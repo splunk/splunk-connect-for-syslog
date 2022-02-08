30b1c2a326f5fa41605ec7cc8c7eb0f87956b3cf
rfaircloth-splunk <rfaircloth@splunk.com>
2022-02-08 08:42:13 -0500

    feat: Support netscout arbor edge
    fixes #1433 Supporting recommended CEF mode only

---
+++ tests/test_netscout_arbor-edge.py
@@ -0,0 +1,45 @@
+# Copyright 2019 Splunk, Inc.
+#
+# Use of this source code is governed by a BSD-2-clause-style
+# license that can be found in the LICENSE-BSD2 file or at
+# https://opensource.org/licenses/BSD-2-Clause
+import random
+
+from jinja2 import Environment
+
+from .sendmessage import *
+from .splunkutils import *
+from .timeutils import *
+
+env = Environment()
+
+
+def test_netscout_arboredge(record_property, setup_wordlist, setup_splunk, setup_sc4s):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    dt = datetime.datetime.now()
+    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
+
+    # Tune time functions
+    epoch = epoch[:-7]
+    epochlong = int(epoch) * 1000
+
+    mt = env.from_string(
+        "{{ mark }} {{ bsd }} {{ host }} CEF:0|NETSCOUT|Arbor Edge Defense|6.8.1.0|TCP Connection Limiting|Blocked Host|5|rt={{ epochlong }} src=190.0.0.0 dpt=25 cn2=109 proto=TCP dst=8.8.8.8 spt=8477 cs2Label=Protection Group Name cn2Label=Protection Group ID cs2=Test_MAIL\n"
+    )
+    message = mt.render(mark="<111>", bsd=bsd, host=host, epochlong=epochlong)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search _time={{ epoch }} index=netids host="{{ host }}" sourcetype="netscout:aed"'
+    )
+    search = st.render(epoch=epoch, host=host)
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
