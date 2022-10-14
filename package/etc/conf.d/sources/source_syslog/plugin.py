dbb03983c8a3ea82f8f5556eea44c31ca72536ef
mbukowski <mbukowski@splunk.com>
2022-10-14 17:21:58 +0200

    Init refactoring

--- package/etc/conf.d/sources/source_syslog/plugin.py
+++ package/etc/conf.d/sources/source_syslog/plugin.py
@@ -9,53 +9,53 @@ templateEnv = jinja2.Environment(loader=templateLoader)
 tm = templateEnv.get_template("plugin.jinja")
 
 
-def initial_setup_from_getenv():
-    enable_ipv6 = "4"
-    store_raw_message = False
-    use_reverse_dns = False
-    use_name_cache = False
-    use_vps_cache = False
-    use_tls = False
-    use_proxy_connect = False
-
+def create_template_params_dict():
     true_list_condition = ["true", "1", "t", "y", "yes"]
+    params = dict()
+    params['enable_ipv6'] = 4
+    params['store_raw_message'] = False
+    params['use_reverse_dns'] = False
+    params['use_name_cache'] = False
+    params['use_vps_cache'] = False
+    params['use_tls'] = False
+    params['use_proxy_connect'] = False
+    params['cert_file'] = "server.pem"
+    params['key_file'] = "server.key"
+    params['all_set_ports'] = os.getenv(f"SOURCE_ALL_SET")
 
     if os.getenv(f"SC4S_IPV6_ENABLE", "no").lower() in true_list_condition:
-        enable_ipv6 = "6"
+        params['enable_ipv6'] = 6
 
     if os.getenv(f"SC4S_SOURCE_STORE_RAWMSG", "no").lower() in true_list_condition:
-        store_raw_message = True
+        params['store_raw_message'] = True
 
     if os.getenv(f"SC4S_USE_REVERSE_DNS", "no").lower() in true_list_condition:
-        use_reverse_dns = True
+        params['use_reverse_dns'] = True
 
     if os.getenv(f"SC4S_USE_NAME_CACHE", "no").lower() in true_list_condition:
-        use_name_cache = True
+        params['use_name_cache'] = True
 
     if os.getenv(f"SC4S_USE_VPS_CACHE", "no").lower() in true_list_condition:
-        use_vps_cache = True
+        params['use_vps_cache'] = True
 
     if os.getenv(f"SC4S_SOURCE_TLS_ENABLE", "no").lower() in true_list_condition:
-        use_tls = True
+        params['use_tls'] = True
 
     if os.getenv(f"SC4S_SOURCE_PROXYCONNECT", "no").lower() in true_list_condition:
-        use_proxy_connect = True
+        params['use_proxy_connect'] = True
 
     if os.getenv(f"SC4S_RUNTIME_ENV", "unknown").lower() == "k8s":
-        cert_file = "tls.crt"
-        key_file = "tls.key"
-    else:
-        cert_file = "server.pem"
-        key_file = "server.key"
+        params['cert_file'] = "tls.crt"
+        params['key_file'] = "tls.key"
 
-    return enable_ipv6, store_raw_message, use_reverse_dns, use_name_cache, use_vps_cache, use_tls, cert_file, key_file, use_proxy_connect
+    return params
 
 
-def render_template_for(ports, enable_ipv6, store_raw_message, use_reverse_dns, use_name_cache,
-                        use_vps_cache, use_tls, cert_file, key_file, use_proxy_connect):
+def render_template_for(template_params):
+    params = template_params;
     vendor: None
     product: None
-    for port_id in ports.split(","):
+    for port_id in template_params['all_set_ports'].split(","):
         if port_id != "DEFAULT":
             port_parts = port_id.split('_', maxsplit=2)
             if len(port_parts) == 2:
@@ -66,17 +66,17 @@ def render_template_for(ports, enable_ipv6, store_raw_message, use_reverse_dns, 
         template = tm.render(
             vendor=vendor,
             product=product,
-            enable_ipv6=enable_ipv6,
-            store_raw_message=store_raw_message,
+            enable_ipv6=params['enable_ipv6'],
+            store_raw_message=params['store_raw_message'],
             port_id=port_id,
-            use_reverse_dns=use_reverse_dns,
-            use_namecache=use_name_cache,
-            use_vpscache=use_vps_cache,
-            use_tls=use_tls,
-            use_proxy_connect=use_proxy_connect,
+            use_reverse_dns=params['use_reverse_dns'],
+            use_namecache=params['use_name_cache'],
+            use_vpscache=params['use_vps_cache'],
+            use_tls=params['use_tls'],
+            use_proxy_connect=params['use_proxy_connect'],
             tls_dir=os.getenv(f"SC4S_TLS", "/etc/syslog-ng/tls"),
-            cert_file=cert_file,
-            key_file=key_file,
+            cert_file=params['cert_file'],
+            key_file=params['key_file'],
             topic=os.getenv(f"SC4S_LISTEN_{port_id}_TOPIC", "sc4s"),
             port_udp=os.getenv(f"SC4S_LISTEN_{port_id}_UDP_PORT", "disabled").split(","),
             port_udp_sockets=int(os.getenv(f"SC4S_SOURCE_LISTEN_UDP_SOCKETS", 4)),
@@ -136,8 +136,6 @@ def render_template_for(ports, enable_ipv6, store_raw_message, use_reverse_dns, 
         return template
 
 
-should_enable_ipv6, should_store_raw_message, should_use_reverse_dns, should_use_name_cache, should_use_vps_cache, should_use_tls, cert_file_name, key_file_name, should_use_proxy_connect = initial_setup_from_getenv()
-
-all_set_ports = os.getenv(f"SOURCE_ALL_SET")
-template = render_template_for(all_set_ports, should_enable_ipv6,  should_store_raw_message, should_use_reverse_dns, should_use_name_cache, should_use_vps_cache, should_use_tls, cert_file_name, key_file_name, should_use_proxy_connect)
+template_params = create_template_params_dict()
+template = render_template_for(template_params)
 print(template)
