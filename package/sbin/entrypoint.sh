b5f9a55c950a86b65d4054e0a76e8b278df57715
Rahul Jha <90039710+rjha-splunk@users.noreply.github.com>
2023-01-19 13:20:54 +0100

    fix: splunk_metadata.csv configuration for helm (#1985)

--- package/sbin/entrypoint.sh
+++ package/sbin/entrypoint.sh
@@ -86,7 +86,9 @@ then
   mkdir -p $SC4S_ETC/conf.d/configmap/config/app_parsers/
 
   # copy all files in configmap/context to conf.d/local/context
+
   cp -R -f $SC4S_ETC/conf.d/configmap/context/* $SC4S_ETC/conf.d/local/context/
+  cp -f $SC4S_ETC/conf.d/configmap/context/splunk_metadata.csv $SC4S_ETC/conf.d/local/context/splunk_metadata.csv
   # copy all files in configmap/config to conf.d/local/config/app_parsers
   cp -R -f $SC4S_ETC/conf.d/configmap/config/* $SC4S_ETC/conf.d/local/config/app_parsers/
 else
