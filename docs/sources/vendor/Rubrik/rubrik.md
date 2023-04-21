295c23b6f23fa3baf0ee2661fb3f5c11f54e2f24
bparmar-splunk <bparmar@splunk.com>
2023-04-21 14:52:11 +0530

    Commvault parser, Rubrik parser & HP ILO parser fixed
    - Commvault parser added
    - Rubrik official support added
    - HP ILO parser fixed
    - Host overwrite in CEF parser removed

---
+++ docs/sources/vendor/Rubrik/rubrik.md
@@ -0,0 +1,25 @@
+# Rubrik
+
+## Key facts
+
+* Default port 514
+
+
+## Links
+
+| Ref            | Link                                                                                                    |
+|----------------|---------------------------------------------------------------------------------------------------------|
+| Splunk Add-on  | None                                                               |
+| Product Manual | None |
+
+## Sourcetypes
+
+| sourcetype      | notes                                                                                                   |
+|-----------------|---------------------------------------------------------------------------------------------------------|
+|rubrik:syslog   |  none  |
+
+## Sourcetype and Index Configuration
+
+| key    | sourcetype     | index  | notes          |
+|--------|----------------|--------|----------------|
+| rubrik_syslog       |  rubrik:syslog  | netops  | None     |
---
+++ tests/test_rubrik.py
@@ -0,0 +1,43 @@
+# Copyright 2019 Splunk, Inc.
+#
+# Use of this source code is governed by a BSD-2-clause-style
+# license that can be found in the LICENSE-BSD2 file or at
+# https://opensource.org/licenses/BSD-2-Clause
+
+import random
+from jinja2 import Environment
+
+from .sendmessage import *
+from .splunkutils import *
+from .timeutils import *
+
+env = Environment()
+
+def test_rubrik_event(record_property, setup_wordlist, setup_splunk, setup_sc4s):
+    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+    dt = datetime.datetime.now()
+    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
+
+    epoch = epoch[:-7]
+
+    mt = env.from_string(
+        "{{ mark }} {{ bsd }} {{ host }} sudo:     XXXXX : TTY=unknown ; PWD=/opt/rubrik/src/java/sd ; USER=XXXXX ; ENV=PYTHONPATH=/opt/rubrik/src ; COMMAND=/opt/rubrik/src/py/forge/sdservice.py cloud-storage-service status"
+    )
+    message = mt.render(
+        mark="<165>", bsd=bsd, host=host, date=date, time=time, tzoffset=tzoffset
+    )
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search _time={{ epoch }} index=netops host="{{ host }}" sourcetype="rubrik:syslog"'
+    )
+    search = st.render(epoch=epoch, host=host)
+
+    resultCount, eventCount = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", resultCount)
+    record_property("message", message)
+
+    assert resultCount == 1
