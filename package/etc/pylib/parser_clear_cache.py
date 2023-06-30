170fb8a54f5e834da8c3bfe28a6ab75aaabd8783
bparmar-splunk <bparmar@splunk.com>
2023-06-30 11:53:37 +0530

    parser_clear_cache file updated.

--- package/etc/pylib/parser_clear_cache.py
+++ package/etc/pylib/parser_clear_cache.py
@@ -1,41 +1,26 @@
 
-import sys
-import traceback
-import socket
-import struct
+import os
 
-import time
 try:
     import syslogng
 except:
     pass
 
-# def ip2int(addr):
-#     return struct.unpack("!I", socket.inet_aton(addr))[0]
-
-# def int2ip(addr):
-#     return socket.inet_ntoa(struct.pack("!I", addr))
-
 hostdict = str("/var/lib/syslog-ng/hostip.sqlite")
 
-class clear_name_cache(object):
-    def init(self, options):
-        self.logger = syslogng.Logger()
-        # self.db = SqliteDict(f"{hostdict}.sqlite")
-        return True
 
-    # def deinit(self):
-    #     self.db.close()
+class clear_name_cache(object):
 
     def parse(self, log_message):
 
         try:
-            if os.path.isfile(hostdict):
-                os.remove(hostdict)
-                sample_file = open('/var/lib/syslog-ng/after_call','w+')
-                sample_file.close()
+            self.logger = syslogng.Logger()
+            sample_file = open('/var/lib/syslog-ng/after_call.txt', 'w+')
+            sample_file.close()
+            # if os.path.isfile(hostdict):
+            #     os.remove(hostdict)
         except:
-            pass
+            return False
 
-        self.logger.debug(f'FixHostResolver::parse method executed')
+        self.logger.debug(f'hostip file')
         return True
