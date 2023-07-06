0fde6194e2ffe02e2d9a49f752fb1ce6d5f5d144
mateuszpierzchala-splunk <mpierzchaa@splunk.com>
2023-07-06 12:59:20 +0200

    feat: Enable new version of syslog, ebpf and sc4s lite

--- package/etc/conf.d/sources/source_syslog/plugin.py
+++ package/etc/conf.d/sources/source_syslog/plugin.py
@@ -10,6 +10,21 @@ tm = templateEnv.get_template("plugin.jinja")
 
 ports = os.getenv(f"SOURCE_ALL_SET")
 
+
+def normalize_env_variable_input(env_variable: str):
+    if os.getenv(env_variable, "no").lower() in [
+        "true",
+        "1",
+        "t",
+        "y",
+        "yes",
+    ]:
+        normalized_value = True
+    else:
+        normalized_value = False
+    return normalized_value
+
+
 if os.getenv(f"SC4S_IPV6_ENABLE", "no").lower() in [
     "true",
     "1",
@@ -21,76 +36,6 @@ if os.getenv(f"SC4S_IPV6_ENABLE", "no").lower() in [
 else:
     enable_ipv6 = "4"
 
-if os.getenv(f"SC4S_SOURCE_STORE_RAWMSG", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    store_raw_message = True
-else:
-    store_raw_message = False
-
-if os.getenv(f"SC4S_USE_REVERSE_DNS", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_reverse_dns = True
-else:
-    use_reverse_dns = False
-
-#SC4S_SOURCE_UDP_IW_USE
-
-if os.getenv(f"SC4S_SOURCE_UDP_IW_USE", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_udp_log_iw = True
-else:
-    use_udp_log_iw = False
-
-#SC4S_USE_NAME_CACHE
-    
-if os.getenv(f"SC4S_USE_NAME_CACHE", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_namecache = True
-else:
-    use_namecache = False
-
-#SC4S_USE_VPS_CACHE
-if os.getenv(f"SC4S_USE_VPS_CACHE", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_vpscache = True
-else:
-    use_vpscache = False
-
-if os.getenv(f"SC4S_SOURCE_TLS_ENABLE", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_tls = True
-else:
-    use_tls = False
 
 if os.getenv(f"SC4S_RUNTIME_ENV", "unknown").lower() == "k8s":
     cert_file = "tls.crt"
@@ -99,24 +44,13 @@ else:
     cert_file = "server.pem"
     key_file = "server.key"
 
-#
-if os.getenv(f"SC4S_SOURCE_PROXYCONNECT", "no").lower() in [
-    "true",
-    "1",
-    "t",
-    "y",
-    "yes",
-]:
-    use_proxy_connect = True
-else:
-    use_proxy_connect = False
 
 for port_id in ports.split(","):
     vendor = None
     product = None
     if port_id != "DEFAULT":
-        port_parts = port_id.split('_',maxsplit=3)
-        if len(port_parts)==2 or len(port_parts)==3:
+        port_parts = port_id.split("_", maxsplit=3)
+        if len(port_parts) == 2 or len(port_parts) == 3:
             vendor = port_parts[0].lower()
             product = port_parts[1].lower()
         else:
@@ -126,17 +60,18 @@ for port_id in ports.split(","):
         vendor=vendor,
         product=product,
         enable_ipv6=enable_ipv6,
-        store_raw_message=store_raw_message,
+        store_raw_message=normalize_env_variable_input(f"SC4S_SOURCE_STORE_RAWMSG"),
         port_id=port_id,
-        use_reverse_dns=use_reverse_dns,
-        use_udp_log_iw=use_udp_log_iw,
-        use_namecache=use_namecache,
-        use_vpscache=use_vpscache,
-        use_tls=use_tls,
-        use_proxy_connect=use_proxy_connect,
+        use_reverse_dns=normalize_env_variable_input(f"SC4S_USE_REVERSE_DNS"),
+        use_udp_log_iw=normalize_env_variable_input(f"SC4S_SOURCE_UDP_IW_USE"),
+        use_namecache=normalize_env_variable_input(f"SC4S_USE_NAME_CACHE"),
+        use_vpscache=normalize_env_variable_input(f"SC4S_USE_VPS_CACHE"),
+        use_tls=normalize_env_variable_input(f"SC4S_SOURCE_TLS_ENABLE"),
+        use_proxy_connect=normalize_env_variable_input(f"SC4S_SOURCE_PROXYCONNECT"),
         tls_dir=os.getenv(f"SC4S_TLS", "/etc/syslog-ng/tls"),
         cert_file=cert_file,
         key_file=key_file,
+        enable_ebpf=normalize_env_variable_input(f"SC4S_ENABLE_EBPF"),
         topic=os.getenv(f"SC4S_LISTEN_{ port_id }_TOPIC", "sc4s"),
         port_udp=os.getenv(f"SC4S_LISTEN_{ port_id }_UDP_PORT", "disabled").split(","),
         port_udp_sockets=int(os.getenv(f"SC4S_SOURCE_LISTEN_UDP_SOCKETS", 4)),
@@ -145,7 +80,7 @@ for port_id in ports.split(","):
         port_tcp_sockets=int(os.getenv(f"SC4S_SOURCE_LISTEN_TCP_SOCKETS", 1)),
         port_tcp_max_connections=os.getenv(f"SC4S_SOURCE_TCP_MAX_CONNECTIONS", "2000"),
         port_udp_log_iw_size=os.getenv(f"SC4S_SOURCE_UDP_IW_SIZE", "250000"),
-        port_udp_log_fetch_limit=os.getenv(f"SC4S_SOURCE_UDP_FETCH_LIMIT", "1000"), 
+        port_udp_log_fetch_limit=os.getenv(f"SC4S_SOURCE_UDP_FETCH_LIMIT", "1000"),
         port_tcp_log_iw_size=os.getenv(f"SC4S_SOURCE_TCP_IW_SIZE", "20000000"),
         port_tcp_log_fetch_limit=os.getenv(f"SC4S_SOURCE_TCP_FETCH_LIMIT", "2000"),
         port_tcp_so_recvbuff=int(os.getenv(f"SC4S_SOURCE_TCP_SO_RCVBUFF", -1)),
@@ -194,5 +129,6 @@ for port_id in ports.split(","):
             f"SC4S_SOURCE_RFC5425_CIPHER_SUITE",
             "HIGH:!aNULL:!eNULL:!kECDH:!aDH:!RC4:!3DES:!CAMELLIA:!MD5:!PSK:!SRP:!KRB5:@STRENGTH",
         ),
+        ebpf_no_sockets=os.getenv(f"SC4S_EBPF_NO_SOCKETS", 1),
     )
     print(outputText)
