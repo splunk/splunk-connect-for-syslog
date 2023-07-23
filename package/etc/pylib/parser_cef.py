1cdc40065bd263c96deda1c9d61ce2e104d83e7f
Ilya Kheifets <ikheifets@splunk.com>
2023-07-23 16:20:05 +0200

    fix: except block without exception

--- package/etc/pylib/parser_cef.py
+++ package/etc/pylib/parser_cef.py
@@ -5,7 +5,7 @@ import traceback
 try:
     import syslogng
     from syslogng import LogParser
-except:
+except Exception:
 
     class LogParser:
         pass
@@ -45,7 +45,7 @@ class cef_kv(LogParser):
                 kc = k.replace(" ", "_").replace(".", "_")
                 log_message[f".values.{kc}"] = v
 
-        except:
+        except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
             self.logger.debug("".join("!! " + line for line in lines))
