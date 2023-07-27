ab4be5a908d951881e89ef85643428f4ddee9e0b
mstopa-splunk <mstopa@splunk.com>
2023-07-27 11:41:26 +0000

    chore: update test samples, product name and filter for Barracuda WAF

--- tests/test_barracuda_waf.py
+++ tests/test_barracuda_waf.py
@@ -14,38 +14,34 @@ from .timeutils import *
 
 import pytest
 
+# Log examples based on https://campus.barracuda.com/product/webapplicationfirewall/doc/92767349/exporting-log-formats/
 test_data = [
     {
-        "orig": "<166>2014-05-20 00:54:44.627 -0700  WAF1 SYS ADMIN_M ALER 51001 Account has been locked for user Kevin because the number of consecutive log-in failures exceeded the maximum allowed",
-        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} ADMIN_M ALER 51001 Account has been locked for user Kevin because the number of consecutive log-in failures exceeded the maximum allowed",
+        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} ADMIN_M ALER 51001 Account has been locked for user User because the number of consecutive log-in failures exceeded the maximum allowed",
         "log_type": "SYS",
         "sourcetype": "barracuda:system",
         "index": "netwaf"
     },
     {
-        "orig": "<166>2014-04-11 10:50:30.411 +0530  wafbox1 WF ALER PRE_1_0_REQUEST 99.99.1.117 34006 99.99.109.2 80 global GLOBAL LOG NONE [POST /index.cgi] POST 99.99.109.2/index.cgi HTTP REQ-0+RES-0 “Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0”  99.99.1.117 34005 Kevin http://99.99.109.2/index.cgi",
-        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} ALER PRE_1_0_REQUEST 99.99.1.117 34006 99.99.109.2 80 global GLOBAL LOG NONE [POST /index.cgi] POST 99.99.109.2/index.cgi HTTP REQ-0+RES-0 “Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0”  99.99.1.117 34005 Kevin http://99.99.109.2/index.cgi",
+        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} ALER PRE_1_0_REQUEST 1.1.1.1 34006 1.1.1.2 80 global GLOBAL LOG NONE [POST /index.cgi] POST 1.1.1.2/index.cgi HTTP REQ-0+RES-0 “Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0”  1.1.1.117 34005 User http://1.1.1.2/index.cgi",
         "log_type": "WF",
         "sourcetype": "barracuda:waf",
         "index": "netwaf"
     },
     {
-        "orig": "<166>2014-04-11 12:11:24.964 +0530  wafbox1 TR 2001::1:109 80 2001::117 43740 \"-\" \"-\" GET HTTP 2001::1:109 HTTP/1.1 200 2837 232 0 1008 2001::117 80 10 REQ-0+RES-0   SERVER DEFAULT PASSIVE VALID /index.html name=srawat http://2001::1:109/index.cgi namdksih=askdj \"Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0\" 2001::117 43740 John gzip,deflate 2001::128 keep-alive",
-        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} 2001::1:109 80 2001::117 43740 \"-\" \"-\" GET HTTP 2001::1:109 HTTP/1.1 200 2837 232 0 1008 2001::117 80 10 REQ-0+RES-0   SERVER DEFAULT PASSIVE VALID /index.html name=srawat http://2001::1:109/index.cgi namdksih=askdj \"Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0\" 2001::117 43740 John gzip,deflate 2001::128 keep-alive",
+        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} 1001::1:1 80 1001::1 43740 \"-\" \"-\" GET HTTP 1001::1:1 HTTP/1.1 200 2837 232 0 1008 1001::1 80 10 REQ-0+RES-0   SERVER DEFAULT PASSIVE VALID /index.html name=user http://1001::1:1/index.cgi namdksih=azkdz \"Mozilla/5.0 (X11; Linux i686; rv:12.0) Gecko/20100101 Firefox/12.0\" 1001::117 43740 User gzip,deflate 1001::128 keep-alive",
         "log_type": "TR",
         "sourcetype": "barracuda:web",
         "index": "netwaf"
     },
     {
-        "orig": "<166>v=2014-02-24 09:05:17.764 -0800  wafbox1 AUDIT Adam GUI 10.11.18.121 24784 CONFIG 166 config SET virtual_ip_config_address 99.99.130.45 virtual_ip_config_interface \"\" \"WAN\" []",
-        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} Adam GUI 10.11.18.121 24784 CONFIG 166 config SET virtual_ip_config_address 99.99.130.45 virtual_ip_config_interface \"\" \"WAN\" []",
+        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} User GUI 1.1.1.121 24784 CONFIG 166 config SET virtual_ip_config_address 99.99.130.45 virtual_ip_config_interface \"\" \"WAN\" []",
         "log_type": "AUDIT",
         "sourcetype": "barracuda:audit",
         "index": "netwaf"
     },
     {
-        "orig": "<166>2014-05-20 00:56:42.195 -0700  WAF1 NF INFO TCP 99.99.1.117 52676 99.99.79.2 80 ALLOW testacl MGMT/LAN/WAN interface traffic:allow",
-        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} INFO TCP 99.99.1.117 52676 99.99.79.2 80 ALLOW testacl MGMT/LAN/WAN interface traffic:allow",
+        "template": "{{ mark }}{{ iso }}  {{ host }} {{ log_type }} INFO TCP 1.1.1.117 52676 1.1.1.2 80 ALLOW testacl MGMT/LAN/WAN interface traffic:allow",
         "log_type": "NF",
         "sourcetype": "barracuda:firewall",
         "index": "netwaf"
