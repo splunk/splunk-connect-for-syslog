423b9fc424ca5fb85eb62616222abda276a13c15
mstopa-splunk <mstopa@splunk.com>
2023-08-23 13:34:21 +0000

    Fix: fix arista eos miscategorized as cisco ios

--- tests/test_arista.py
+++ tests/test_arista.py
@@ -3,7 +3,7 @@
 # Use of this source code is governed by a BSD-2-clause-style
 # license that can be found in the LICENSE-BSD2 file or at
 # https://opensource.org/licenses/BSD-2-Clause
-import random
+import pytest
 
 from jinja2 import Environment
 
@@ -13,11 +13,15 @@ from .timeutils import *
 
 env = Environment()
 
+test_data = [
+    "{{ mark }} {{ iso }}Z host Acl: %ACL-6-IPACCESS: list acl-internet Ethernet1 denied tcp xxx.xx.xx.xx(63751) -> xxx.xx.xx.xx(445)",
+    "{{ mark }} {{ iso }}Z AleL3Agent-primary: %AGENT-6-INITIALIZED: Agent 'AleL3Agent-primary' initialized; pid=XXXX",
+    "{{ mark }} {{ iso }}Z ProcMgr-worker: %PROCMGR-6-WORKER_WARMSTART: ProcMgr worker warm start. (PID=XXXXX)"
+]
 
-# <111>2021-11-25T16:52:18+01:00 SWITCHNAME.domain.com Acl: %ACL-6-IPACCESS: list acl-internet Ethernet1 denied tcp xxx.xx.xx.xx(63751) -> xxx.xx.xx.xx(445)
-# <111>2021-11-25T16:52:18+01:00 SWITCHNAME.domain.com Acl: 100: %ACL-6-IPACCESS: list acl-internet Ethernet1 denied tcp xxx.xx.xx.xx(63751) -> xxx.xx.xx.xx(445)
-def test_arista_switch(record_property, setup_wordlist, setup_splunk, setup_sc4s):
-    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
+
+@pytest.mark.parametrize("event", test_data)
+def test_arista_switch(record_property, setup_wordlist, setup_splunk, setup_sc4s, event):
 
     #   Get UTC-based 'dt' time structure
     dt = datetime.datetime.now(datetime.timezone.utc)
@@ -28,21 +32,18 @@ def test_arista_switch(record_property, setup_wordlist, setup_splunk, setup_sc4s
     iso = dt.isoformat()[0:19]
     epoch = epoch[:-7]
 
-    mt = env.from_string(
-        "{{ mark }} {{ iso }}Z {{ host }} Acl: %ACL-6-IPACCESS: list acl-internet Ethernet1 denied tcp xxx.xx.xx.xx(63751) -> xxx.xx.xx.xx(445)\n"
-    )
-    message = mt.render(mark="<166>", iso=iso, epoch=epoch, host=host)
+    mt = env.from_string(event + "\n")
+    message = mt.render(mark="<166>", iso=iso, epoch=epoch)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
     st = env.from_string(
-        'search _time={{ epoch }} index=netops host="{{ host }}" sourcetype="arista:eos" source="arista:eos:acl" "ACL-6-IPACCESS"'
+        'search _time={{ epoch }} index=netops sourcetype="arista:eos" source="arista:eos:acl" "ACL-6-IPACCESS"'
     )
-    search = st.render(epoch=epoch, host=host)
+    search = st.render(epoch=epoch)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
 
-    record_property("host", host)
     record_property("resultCount", resultCount)
     record_property("message", message)
 
