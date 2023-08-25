6183880c54f25a23f8f581882c26a0c43c1e2fcc
mstopa-splunk <139441697+mstopa-splunk@users.noreply.github.com>
2023-08-25 11:10:24 +0200

    Fix: fix arista eos miscategorized as cisco ios (#2146)
    * fix: fix arista eos miscategorized as cisco ios

--- tests/test_cisco_ace.py
+++ tests/test_cisco_ace.py
@@ -3,8 +3,6 @@
 # Use of this source code is governed by a BSD-2-clause-style
 # license that can be found in the LICENSE-BSD2 file or at
 # https://opensource.org/licenses/BSD-2-Clause
-import random
-
 from jinja2 import Environment
 
 from .sendmessage import *
@@ -18,8 +16,6 @@ env = Environment()
 def test_cisco_ace_traditional(
     record_property, setup_wordlist, setup_splunk, setup_sc4s
 ):
-    host = "{}-{}".format(random.choice(setup_wordlist), random.choice(setup_wordlist))
-
     dt = datetime.datetime.now()
     iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
 
@@ -27,20 +23,19 @@ def test_cisco_ace_traditional(
     epoch = epoch[:-7]
 
     mt = env.from_string(
-        "{{ mark }} {{ bsd }} {{ host }}: %ACE-3-251010: Health probe failed for server X.X.X.X on port 8000, server reply timeout\n"
+        "{{ mark }} {{ bsd }} 192.168.12.1: %ACE-3-251010: Health probe failed for server X.X.X.X on port 8000, server reply timeout\n"
     )
-    message = mt.render(mark="<111>", bsd=bsd, host=host)
+    message = mt.render(mark="<111>", bsd=bsd)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
     st = env.from_string(
-        'search _time={{ epoch }} index=netops host="{{ host }}" sourcetype="cisco:ace"'
+        'search _time={{ epoch }} index=netops sourcetype="cisco:ace"'
     )
-    search = st.render(epoch=epoch, host=host)
+    search = st.render(epoch=epoch)
 
     resultCount, eventCount = splunk_single(setup_splunk, search)
 
-    record_property("host", host)
     record_property("resultCount", resultCount)
     record_property("message", message)
 
