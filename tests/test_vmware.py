baad3393130bb6f451548e4916429722638197b6
zyphermonkey <zyphermonkey@gmail.com>
2023-11-15 10:40:39 +0000

    remove vmware esx auto-backup.sh test

--- tests/test_vmware.py
+++ tests/test_vmware.py
@@ -565,37 +565,3 @@ def test_vmware_overlapping_with_another_sdata(
     record_property("message", message)
 
     assert result_count == 1
-
-#
-# <7>2023-09-29T12:01:00.124Z esxi01 auto-backup.sh.2904545.[2904558]: ConfigStore has been modified since the last backup
-# <7>2023-09-29T12:01:00.340Z esxi04 auto-backup.sh.2800924.[2800937]: ConfigStore has been modified since the last backup
-def test_linux_vmware_esx_autobackup(record_property, setup_splunk, setup_sc4s, get_pid):
-    host = f"testvmw-{shortuuid.ShortUUID().random(length=5).lower()}-{shortuuid.ShortUUID().random(length=5).lower()}"
-    pid = get_pid
-
-    dt = datetime.datetime.now(datetime.timezone.utc)
-    iso, bsd, time, date, tzoffset, tzname, epoch = time_operations(dt)
-
-    # Tune time functions
-    # iso from included timeutils is from local timezone; need to keep iso as UTC
-    iso_header = dt.isoformat()[0:23]
-    epoch = epoch[:-3]
-
-    mt = env.from_string(
-        "{{ mark }}{{ iso_header }}Z {{ host }} auto-backup.sh.2904545.[{{ pid }}]: ConfigStore has been modified since the last backup\n"
-    )
-    message = mt.render(mark="<7>", iso_header=iso_header, host=host, pid=pid)
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
-
-    st = env.from_string(
-        'search _time={{ epoch }} index=infraops host={{ host }} {{ pid }} sourcetype="vmware:esxlog:auto-backup"'
-    )
-    search = st.render(epoch=epoch, host=host)
-
-    resultCount, eventCount = splunk_single(setup_splunk, search)
-
-    record_property("host", host)
-    record_property("resultCount", resultCount)
-    record_property("message", message)
-
-    assert resultCount == 1
