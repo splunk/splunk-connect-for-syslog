219d94cea42819536c1acd1ad469d69003f7f776
mstopa-splunk <mstopa@splunk.com>
2023-11-30 11:17:10 +0000

    Stabilize unit tests for name cache

--- tests/test_name_cache.py
+++ tests/test_name_cache.py
@@ -7,11 +7,27 @@
 import datetime
 import time
 
+from jinja2 import Environment
 import pytest
 
 from .timeutils import time_operations
 from .sendmessage import sendsingle
 from .splunkutils import  splunk_single
+
+env = Environment()
+
+
+def send_message(message_template, setup_sc4s, host=None):
+    dt = datetime.datetime.now()
+    _, bsd, _, _, _, _, epoch = time_operations(dt)
+    epoch = epoch[:-7]
+
+    mt = env.from_string(message_template)
+    message = mt.render(mark="<111>", bsd=bsd, host=host)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    return epoch
 
 
 @pytest.mark.name_cache
@@ -19,26 +35,17 @@ def test_name_cache(get_host_key, setup_splunk, setup_sc4s):
     """
     Send a log event without a valid host.
     Then send another log event with a valid host to generate name cache entry.
-    Then send the first event again. It should be assigned host value from the cache,
-    so the search result should be equal 2. 
+    Then send the first event again. It should be assigned host value from the cache.
     """
-    dt = datetime.datetime.now()
-    _, bsd, _, _, _, _, epoch = time_operations(dt)
-    epoch = epoch[:-7]
+    template_no_host = "{{ mark }} {{ bsd }} CEF:0|Trend Micro|Deep Security Manager|0.0.0|600|User Signed In|3|src=10.52.116.160|"
+    template_with_host = "{{ mark }} {{ bsd }} {{ host }} CEF:0|Trend Micro|Deep Security Manager|0.0.0|600|User Signed In|3|src=10.52.116.160|"
 
-    message_no_host = f"<111> {bsd} CEF:0|Trend Micro|Deep Security Manager|0.0.0|600|User Signed In|3|src=10.52.116.160"
-    message_with_host = f"<111> {bsd} {get_host_key} CEF:0|Trend Micro|Deep Security Manager|0.0.0|600|User Signed In|3|src=10.52.116.160"
-
-    send_message = lambda message: sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
-
-    send_message(message_no_host)
-    send_message(message_with_host)
-    time.sleep(1) # time to save cache record
-    send_message(message_no_host)
-    time.sleep(1) # splunk_single() will wait for the first record only
-                  # let's make sure that the second one gets time to be processed
+    _ = send_message(template_no_host, setup_sc4s)
+    _ = send_message(template_with_host, setup_sc4s, host=get_host_key)
+    time.sleep(1) # time to save the new cache entry
+    epoch = send_message(template_no_host, setup_sc4s)
 
     search = f'search _time="{epoch}" index=* host="{get_host_key}"'
 
     result_count, _ = splunk_single(setup_splunk, search)
-    assert result_count == 2
+    assert result_count == 1
