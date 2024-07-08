fa53234a2dfedba4454d80211eb5f58ec1c8ded1
Meet Usadadiya <musadadiya@splunk.com>
2024-07-08 17:54:20 +0530

    feat: add udp support

--- tests/sendmessage.py
+++ tests/sendmessage.py
@@ -9,19 +9,24 @@ from time import sleep
 import os
 
 
-def sendsingle(message, host, port):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def sendsingle(message, host, port, use_udp=False):
+
+    socket_type = socket.SOCK_DGRAM if use_udp else socket.SOCK_STREAM
+    sock = socket.socket(socket.AF_INET, socket_type)
     server_address = (host, port)
 
-    tried = 0
-    while True:
-        try:
-            sock.connect(server_address)
-            break
-        except socket:
-            tried += 1
-            if tried > 90:
-                raise
-            sleep(1)
-    sock.sendall(str.encode(message))
+    if use_udp:
+        sock.sendto(str.encode(message), server_address)
+    else:
+        tried = 0
+        while True:
+            try:
+                sock.connect(server_address)
+                break
+            except socket:
+                tried += 1
+                if tried > 90:
+                    raise
+                sleep(1)
+        sock.sendall(str.encode(message))
     sock.close()
