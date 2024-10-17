14f12c443158eec18a8f349fd24e5d2655b5815d
mstopa-splunk <mstopa@splunk.com>
2024-10-17 13:07:07 +0000

    fix: fix sqlitedict CVE

--- package/etc/pylib/parser_vps_cache.py
+++ package/etc/pylib/parser_vps_cache.py
@@ -18,13 +18,47 @@ except Exception:
         pass
 
 
+import builtins
+import io
+import pickle
+from base64 import b64decode
+
+safe_builtins = {
+    'range',
+    'complex',
+    'set',
+    'frozenset',
+    'slice',
+}
+
+class RestrictedUnpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        # Only allow safe classes from builtins.
+        if module == "builtins" and name in safe_builtins:
+            return getattr(builtins, name)
+        # Forbid everything else.
+        raise pickle.UnpicklingError("global '%s.%s' is forbidden" %
+                                     (module, name))
+
+def restricted_loads(s):
+    """Helper function analogous to pickle.loads()."""
+    return RestrictedUnpickler(io.BytesIO(s)).load()
+
+def restricted_decode(obj):
+    """Overwrite sqlitedict.decode to prevent code injection."""
+    return restricted_loads(bytes(obj))
+
+def restricted_decode_key(key):
+    """Overwrite sqlitedict.decode_key to prevent code injection."""
+    return restricted_loads(b64decode(key.encode("ascii")))
+
 hostdict = str("/var/lib/syslog-ng/vps")
 
 
 class vpsc_parse(LogParser):
     def init(self, options):
         self.logger = syslogng.Logger()
-        self.db = SqliteDict(f"{hostdict}.sqlite")
+        self.db = SqliteDict(f"{hostdict}.sqlite", decode=restricted_decode, decode_key=restricted_decode_key)
         return True
 
     def deinit(self):
@@ -52,7 +86,7 @@ class vpsc_dest(LogDestination):
     def init(self, options):
         self.logger = syslogng.Logger()
         try:
-            self.db = SqliteDict(f"{hostdict}.sqlite", autocommit=True)
+            self.db = SqliteDict(f"{hostdict}.sqlite", autocommit=True, decode=restricted_decode, decode_key=restricted_decode_key)
         except Exception:
             exc_type, exc_value, exc_traceback = sys.exc_info()
             lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
