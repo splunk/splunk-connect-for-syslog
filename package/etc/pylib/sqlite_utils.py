27b778614f51900e3c7b1553456487241701e7ba
mstopa-splunk <mstopa@splunk.com>
2024-10-29 13:04:26 +0000

    Add tests for RestrictedSqliteDict

--- package/etc/pylib/sqlite_utils.py
+++ package/etc/pylib/sqlite_utils.py
@@ -1,37 +1,25 @@
-import builtins
 import io
 import pickle
 from base64 import b64decode
 from sqlitedict import SqliteDict
 
-safe_builtins = {
-    'range',
-    'complex',
-    'set',
-    'frozenset',
-    'slice',
-}
-
 
 class RestrictedUnpickler(pickle.Unpickler):
     def find_class(self, module, name):
-        # Only allow safe classes from builtins.
-        if module == "builtins" and name in safe_builtins:
-            return getattr(builtins, name)
-        # Forbid everything else.
-        raise pickle.UnpicklingError("global '%s.%s' is forbidden" %
-                                     (module, name))
+        """Override pickle.Unpickler.find_class() to prevent deserialization of class instances."""
+        raise pickle.UnpicklingError("Class deserialization is disabled")
+
 
 def restricted_loads(s):
     """Helper function analogous to pickle.loads()."""
     return RestrictedUnpickler(io.BytesIO(s)).load()
 
 def restricted_decode(obj):
-    """Overwrite sqlitedict.decode to prevent code injection."""
+    """Overwrite sqlitedict.decode() to prevent code injection."""
     return restricted_loads(bytes(obj))
 
 def restricted_decode_key(key):
-    """Overwrite sqlitedict.decode_key to prevent code injection."""
+    """Overwrite sqlitedict.decode_key() to prevent code injection."""
     return restricted_loads(b64decode(key.encode("ascii")))
 
 
