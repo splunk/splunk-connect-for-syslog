66790253c4e672cc0cbea5feaae7dae9eaab0955
cwadhwani-splunk <cwadhwani@splunk.com>
2024-11-12 17:43:05 +0530

    Updated the documentation and made some changes in the parser

--- docs/sources/vendor/NetApp/ontap.md
+++ docs/sources/vendor/NetApp/ontap.md
@@ -3,7 +3,7 @@
 ## Key facts
 
 * MSG Format based filter
-* Legacy BSD Format default port 514
+* Netapp Ontap messages are not distinctive. So, either configure known Netapp Ontap hosts in SC4S, or open unique ports for Netapp Ontap devices
 
 ## Links
 
@@ -16,11 +16,38 @@
 
 | sourcetype     | notes                                                                                                   |
 |----------------|---------------------------------------------------------------------------------------------------------|
-| netapp:ems  | None |
+| netapp:ontap:audit  | None |
+| netapp:ontap:ems  | None |
 
 ## Sourcetype and Index Configuration
 
 | key            | sourcetype     | index          | notes          |
 |----------------|----------------|----------------|----------------|
-| netapp_ontap      | netapp:ems     | infraops          | none          |
+| netapp_ontap_audit      | netapp:ontap:audit     | infraops          | none          |
+| netapp_ontap_ems      | netapp:ontap:ems     | infraops          | none          |
 
+
+## Parser Configuration
+1. Through sc4s-vps
+```c
+#/opt/sc4s/local/config/app-parsers/app-vps-netapp_ontap.conf
+#File name provided is a suggestion it must be globally unique
+
+application app-vps-test-netapp_ontap[sc4s-vps] {
+    filter { 
+        host("netapp-ontap-" type(string) flags(prefix))
+    }; 
+    parser { 
+        p_set_netsource_fields(
+            vendor('netapp')
+            product('ontap')
+        ); 
+    };   
+};
+```
+
+2. or through unique port
+```
+# /opt/sc4s/env_file 
+SC4S_LISTEN_NETAPP_ONTAP_UDP_PORT=5005
+```
--- tests/test_netapp_ontap.py
+++ tests/test_netapp_ontap.py
@@ -17,16 +17,16 @@ env = Environment(autoescape=select_autoescape(default_for_string=False))
 
 
 testdata = [
-    "{{ mark }}{{ bsd }} {{ host }}: {{ host }}: 0000001e.0794c163 055b6737 {{ device_time }} [kern_audit:info:2385] 8503ea0000ba6b71 :: nodea:ontapi :: 10.10.10.10:41464 :: nodea-esx:usera :: clone-create :: Error: Missing input: source-path; Missing input: volume",
+    "{{ mark }}{{ bsd }} {{ host }}: {{ host }}: 00000030.00c8f1e2 11e5347f {{ device_time }} [kern_audit3167] 8004b7000021e73b:4005f7000021e73d :: cluster:ssh :: 0.0.0.0:32879 :: cluster:admin :: qos statistics volume performance show -rows 20 -iter 1 :: Pending",
 ]
 
-
+# <14>Oct 3 11:36:46 host: host: 00000030.00c8f1e2 11e5347f Thu Oct 03 2024 11:36:44 -06:00 [kern_audit3167] 8004b7000021e73b:4005f7000021e73d :: cluster:ssh :: 0.0.0.0:32879 :: cluster:admin :: qos statistics volume performance show -rows 20 -iter 1 :: Pending
 @pytest.mark.addons("netapp")
 @pytest.mark.parametrize("event", testdata)
-def test_netapp(
+def test_netapp_ontap_audit(
     record_property,  get_host_key, setup_splunk, setup_sc4s, event
 ):
-    host = get_host_key
+    host = "netapp-ontap-" + get_host_key
 
     dt = datetime.datetime.now(datetime.timezone.utc)
     _, bsd, _, _, _, _, epoch = time_operations(dt)
@@ -37,12 +37,12 @@ def test_netapp(
     device_time = dt.strftime("%a %b %d %Y %H:%M:%S +00:00")
 
     mt = env.from_string(event + "\n")
-    message = mt.render(mark="<166>", bsd=bsd, host=host, device_time=device_time)
+    message = mt.render(mark="<14>", bsd=bsd, host=host, device_time=device_time)
 
     sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
 
     st = env.from_string(
-        'search index=infraops _time={{ epoch }} sourcetype="ontap:ems" (host="{{ host }}" OR "{{ host }}")'
+        'search index=infraops _time={{ epoch }} sourcetype="netapp:ontap:ems"'
     )
     search = st.render(epoch=epoch, host=host)
 
@@ -55,42 +55,13 @@ def test_netapp(
     assert result_count == 1
 
 
-def test_netapp_ontap_ems_rfc3164(
-        record_property,  get_host_key, setup_splunk, setup_sc4s
-):
-    host = get_host_key
-
-    dt = datetime.datetime.now(datetime.timezone.utc)
-    _, bsd, _, _, _, _, epoch = time_operations(dt)
-
-    # Tune time functions
-    epoch = epoch[:-7]
-
-    mt = env.from_string(
-        '{{ mark }}{{ bsd }} [cluster-01:secd.conn.auth.failure:notice]: Vserver (datavserver) could not make a connection over the network to server (ip 2.3.3.3, port 389). Error: Operation timed out (Service: LDAP (Active Directory), Operation: SiteDiscovery)'
-    )
-    message = mt.render(mark="<13>", bsd=bsd, host=host)
-
-    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
-
-    st = env.from_string(
-        'search _time={{ epoch }} index=infraops sourcetype="netapp:ontap:ems"'
-    )
-    search = st.render(epoch=epoch, host=host)
-
-    result_count, _ = splunk_single(setup_splunk, search)
-
-    record_property("host", host)
-    record_property("resultCount", result_count)
-    record_property("message", message)
-
-    assert result_count == 1
-
-
+# Netapp Ontap EMS event in rfc5424 format
+# <5>1 2024-10-03T07:54:02-06:00 host program - wafl.scan.done - Completed Volume Footprint Estimator Scan on volume vm_unix002_0d@vserver:27902083bf98-11e9-87fe-00a098b15eb6
+@pytest.mark.addons("netapp")
 def test_netapp_ontap_ems_rfc5424(
         record_property,  get_host_key, setup_splunk, setup_sc4s
 ):
-    host = get_host_key
+    host = "netapp-ontap-" + get_host_key
 
     dt = datetime.datetime.now(datetime.timezone.utc)
     iso, _, _, _, _, _, epoch = time_operations(dt)
@@ -99,7 +70,7 @@ def test_netapp_ontap_ems_rfc5424(
     epoch = epoch[:-7]
 
     mt = env.from_string(
-        '{{ mark }} {{ iso }} {{ host }} kernel - wafl.scan.done - Completed Volume Footprint Estimator Scan on volume vm_unix002_0d@vserver:27902083bf98-11e9-87fe-00a098b15eb6'
+        '{{ mark }} {{ iso }} {{ host }} program - wafl.scan.done - Completed Volume Footprint Estimator Scan on volume vm_unix002_0d@vserver:27902083bf98-11e9-87fe-00a098b15eb6'
     )
     message = mt.render(mark="<5>1", iso=iso, host=host)
 
