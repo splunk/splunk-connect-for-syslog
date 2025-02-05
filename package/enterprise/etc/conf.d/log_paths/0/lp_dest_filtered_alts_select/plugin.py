f17a9185129eb130c38bdf2c09993dbad326082b
Rahul Jha <90039710+rjha-splunk@users.noreply.github.com>
2025-02-05 17:01:50 +0100

    feat:New Enterprise version (#2656)
    * feat:New Enterprise version
    
    * feat:New Enterprise
    
    * feat:New Enterprise update
    
    * feat:New Enterprise update
    
    * feat:New Enterprise update
    
    * feat:New Enterprise update
    
    * feat:New Enterprise update
    
    * feat:New Enterprise update
    
    * feat: enterprise release workflow file
    
    * feat: enterprise release workflow file
    
    * feat: enterprise release workflow file
    
    * Update cd-ghcr.io.yaml
    
    * Update ci-enterprise.yaml
    
    * feat: enterprise release workflow file
    
    * Update ci-enterprise.yaml
    
    * feat: enterprise release workflow file
    
    * Update ci-enterprise.yaml
    
    * feat: enterprise release workflow file
    
    * Update ci-enterprise.yaml
    
    * Update ci-enterprise.yaml
    
    * Update .releaserc
    
    * Update .releaserc
    
    * feat: enterprise release workflow file
    
    * Update ci-enterprise.yaml
    
    * feat: enterprise release workflow file
    
    * feat: enterprise release workflow file
    
    * Update ci-enterprise.yaml
    
    * Update ci-enterprise.yaml
    
    * Re Update ci-enterprise.yaml
    
    * Re Update ci-enterprise.yaml release workflow
    
    * re Update update-tags.yaml

---
+++ package/enterprise/etc/conf.d/log_paths/0/lp_dest_filtered_alts_select/plugin.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python3
+import os
+import jinja2
+import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+plugin_path = os.path.dirname(os.path.abspath(__file__))
+
+templateLoader = jinja2.FileSystemLoader(searchpath=plugin_path)
+templateEnv = jinja2.Environment(
+    loader=templateLoader,
+    autoescape=jinja2.select_autoescape(default_for_string=False),
+)
+tm = templateEnv.get_template("plugin.jinja")
+
+
+for var in os.environ:
+    routes = {}
+    filters = {}
+    if var.startswith("SC4S_DEST_") and var.endswith("_FILTERED_ALTERNATES"):
+        dest_key = var.replace("SC4S_DEST_", "").replace("_FILTERED_ALTERNATES", "")
+        lower_dest_key = dest_key.lower()
+        dest_key_dests = os.environ[var].split(",")
+        dest_filters = os.getenv(
+            f"SC4S_DEST_{ dest_key }_ALT_FILTER", "f_is_nevermatch"
+        ).split(",")
+        # create a list of all the dests
+        pairs = []
+        filters = {}
+        for i in range(0, len(dest_key_dests)):
+            d = dest_key_dests[i]
+            # dest_key_dests[i]
+            if len(dest_filters) == 1:
+                f = dest_filters[0]
+            else:
+                f = dest_filters[i]
+
+            if f in filters.keys():
+                filters[f].append(d)
+            else:
+                filters[f] = [d]
+        msg = tm.render(dest_key=dest_key, filters=filters, lower_dest_key=lower_dest_key)
+        print(msg)
