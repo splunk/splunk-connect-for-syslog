136ad16e804222c56db8dbf325d850d60dbb09d7
Kawron <kwrona@cisco.com>
2026-04-01 10:28:07 +0200

    feat: add the test case for cef compliant log

--- tests/test_guardicore.py
+++ tests/test_guardicore.py
@@ -58,3 +58,38 @@ def test_guardicore_centra_almost_syslog(record_property, get_host_key, setup_sp
     record_property("message", message)
 
     assert result_count == 1
+
+
+@pytest.mark.addons("guardicore")
+def test_guardicore_centra_cef(record_property, get_host_key, setup_splunk, setup_sc4s):
+    host = "guardicore-" + get_host_key
+
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    _, bsd, _, _, _, _, epoch = time_operations(dt)
+
+    epoch = epoch[:-7]
+
+    mt = env.from_string(
+        "<14>{{ bsd }} {{ host }} CEF:0|Guardicore|Centra|51|Network Log|Network Log|None|"
+        "id=cda17db8 act=Allowed cnt=1 src=1.1.1.1 shost=shost "
+        "dst=1.1.1.1 dpt=53 dhost=Unknown proto=TCP cs1Label=connection_type cs1=SUCCESSFUL "
+        "cs2Label=source_asset_labels cs2=os_type: AIX cs6Label=connection_verdict cs6=allowed "
+        "cs7Label=policy_rule cs7=cs7 "
+        "cs8Label=policy_ruleset cs8=AIX DNS Control"
+    )
+    message = mt.render(bsd=bsd, host=host)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search index=netops sourcetype="guardicore:centra:cef" host="{{ host }}" earliest={{ epoch }}'
+    )
+    search = st.render(host=host, epoch=epoch)
+
+    result_count, _ = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", result_count)
+    record_property("message", message)
+
+    assert result_count == 1
