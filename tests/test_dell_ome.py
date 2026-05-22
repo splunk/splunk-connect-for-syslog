d21fee1f9f51355c2807a0db334130c760a85b61
digrajpu <digrajpu@cisco.com>
2026-05-22 11:16:36 +0530

    feat: Adding support to dell OME product (#3067)
    * feat: Adding support to dell OME product
    
    * feat: Updated the filtering logic to improve pattern match
    
    * feat: Single order regex pattern
    
    * feat: replacing space char with one space
    
    * feat: Adding doc for dell ome vendor
    
    * feat: Change regex in lite package
    
    * feat: Changes in parser after analysing the pcap file.

---
+++ tests/test_dell_ome.py
@@ -0,0 +1,52 @@
+# Copyright 2026 Splunk, Inc.
+#
+# Use of this source code is governed by a BSD-2-clause-style
+# license that can be found in the LICENSE-BSD2 file or at
+# https://opensource.org/licenses/BSD-2-Clause
+
+from jinja2 import Environment, select_autoescape
+import shortuuid
+
+from .sendmessage import sendsingle
+from .splunkutils import splunk_single
+from .timeutils import time_operations
+import datetime
+
+
+import pytest
+
+env = Environment(autoescape=select_autoescape(default_for_string=False))
+
+
+test_cases = [
+    "{{ mark }}{{ bsd }} ttpva-ome1 EEMI Audit event from device with { IP } 1.1.1.1 { HostName } {{ host }} { Severity } Warning { MessageID } MSG0001 { Message } Description: Login attempt alert for OME_c3b3edfr4 from 192.168.0.0.1 using REDFISH, IP will be blocked for 60 seconds. - System Display Name: iDRAC - System Service Tag: N97D5 - FQDN: {{ host }} - FQDD: iDRAC.Embedded.1 - Chassis Service Tag: N97D5  { Recommended Action } Contact the iDRAC administrator and make sure the username and password credentials used are correct. Check the Lifecycle Controller Log (LC Log) to see if more unauthorized iDRAC access attempts are occurring than would be expected due to forgotten account names or passwords.",
+]
+
+
+@pytest.mark.parametrize("case", test_cases)
+@pytest.mark.addons("dell")
+def test_dell_ome(record_property, setup_splunk, setup_sc4s, case):
+    host = f"{shortuuid.ShortUUID().random(length=5).lower()}-{shortuuid.ShortUUID().random(length=5).lower()}"
+
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    _, bsd, _, date, _, _, epoch = time_operations(dt)
+
+    # Tune time functions
+    epoch = epoch[:-7]
+
+    mt = env.from_string(case + "\n")
+    message = mt.render(mark="<156>", bsd=bsd, host=host)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search index=infraops _time={{ epoch }} sourcetype="dell:ome" (host="{{ host }}" OR "{{ host }}")'
+    )
+    search = st.render(epoch=epoch, host=host)
+    result_count, _ = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", result_count)
+    record_property("message", message)
+
+    assert result_count == 1
