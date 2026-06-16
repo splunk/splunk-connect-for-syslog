cbc3ccb330e3ed01753298033769df696300161c
digrajpu <digrajpu@cisco.com>
2026-06-16 13:27:01 +0530

    fix: ILO 5 metadata issue (#3092)
    * fix: ILO 5 metadata issue
    
    * fix: updating lite ilo parser
    
    * fix: Add support for HPE iLO 5 syslog format while preserving old parsing
    
    * fix: Code formatting fixes

--- tests/test_hpe_ilo.py
+++ tests/test_hpe_ilo.py
@@ -11,7 +11,7 @@ import datetime
 
 env = Environment(autoescape=select_autoescape(default_for_string=False))
 
-# <134>1 2022-01-27T19:51:46Z host #ILO4 - - - Browser login: Administrator - 10.0.0.0(host.domain.local)
+#<134>1 2022-01-27T19:51:46Z host #ILO4 - - - Browser login: Administrator - 10.0.0.0(host.domain.local)
 @pytest.mark.addons("hp")
 def test_hpe_ilo_4(
     record_property,  get_host_key, setup_splunk, setup_sc4s
@@ -27,6 +27,41 @@ def test_hpe_ilo_4(
 
     mt = env.from_string(
         "{{ mark }} {{ iso }}Z {{ host }} #ILO4 - - - Browser login: Administrator - 10.0.0.0(host.domain.local)\n"
+    )
+    message = mt.render(mark="<165>1", iso=iso, host=host)
+
+    sendsingle(message, setup_sc4s[0], setup_sc4s[1][514])
+
+    st = env.from_string(
+        'search _time={{ epoch }} index=infraops host="{{ host }}" sourcetype="hpe:ilo"'
+    )
+    search = st.render(epoch=epoch, host=host)
+
+    result_count, _ = splunk_single(setup_splunk, search)
+
+    record_property("host", host)
+    record_property("resultCount", result_count)
+    record_property("message", message)
+
+    assert result_count == 1
+
+
+# <134>1 2022-01-27T19:51:46Z host  DriveArray: Smart Array - Drive is failed...
+@pytest.mark.addons("hp")
+def test_hpe_ilo_5(
+    record_property,  get_host_key, setup_splunk, setup_sc4s
+):
+    host = get_host_key
+
+    dt = datetime.datetime.now(datetime.timezone.utc)
+    iso, _, _, _, _, _, epoch = time_operations(dt)
+
+    # Tune time functions
+    iso = dt.isoformat()[0:23]
+    epoch = epoch[:-3]
+
+    mt = env.from_string(
+        "{{ mark }} {{ iso }}Z {{ host }} DriveArray: Smart Array - Drive is failed...\n"
     )
     message = mt.render(mark="<165>1", iso=iso, host=host)
 
